0e24805151bc712194b091c11d93abfa325fcd7f
AvaN0x <clement.ricatte57@gmail.com>
2020-09-03 11:34:15 +0200

    remove something useless

--- resources/[esx]/[farming]/mine/item.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
-('stone', 'Pierre', 100, 0, 1),
-('copper', 'Cuivre', 100, 0, 1),
-('iron', 'Fer', 50, 0, 1),
-('gold', 'Or', 25, 0, 1),
-('drill', 'Perceuse',1, 0, 1);
