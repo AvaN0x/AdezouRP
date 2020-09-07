3b7a8b6cd78fe3353492d5c25191d2bdc5c34f3c
AvaN0x <clement.ricatte57@gmail.com>
2020-09-07 22:06:39 +0200

    Add speedometer to avan0x_hud (WIP seatbelt)

--- resources/[hud]/avan0x_hud/client.lua
+++ resources/[hud]/avan0x_hud/client.lua
@@ -77,3 +77,36 @@ RegisterNetEvent('esx_customui:updateStatus')
 AddEventHandler('esx_customui:updateStatus', function(status)
 	SendNUIMessage({action = "updateStatus", status = status})
 end)
+
+
+Citizen.CreateThread(function()
+	while true do
+		local Ped = GetPlayerPed(-1)
+		if(IsPedInAnyVehicle(Ped)) then
+			local PedCar = GetVehiclePedIsIn(Ped, false)
+			if PedCar and GetPedInVehicleSeat(PedCar, -1) == Ped then
+				-- Speed
+				carSpeed = math.ceil(GetEntitySpeed(PedCar) * 3.6)
+				fuel = GetVehicleFuelLevel(PedCar)
+				SendNUIMessage({
+					action = 'showcarhud', 
+					showhud = true,
+					speed = carSpeed,
+					fuel = fuel
+				})
+			else
+				SendNUIMessage({
+					action = 'showcarhud', 
+					showhud = false
+				})
+			end
+		else
+			SendNUIMessage({
+				action = 'showcarhud', 
+				showhud = false
+		})
+		end
+
+		Citizen.Wait(1)
+	end
+end)
--- resources/[hud]/avan0x_hud/html/main.js
+++ resources/[hud]/avan0x_hud/html/main.js
@@ -1,7 +1,4 @@
 ﻿
-// var rgbStart = [139,195,74]
-// var rgbEnd = [183,28,28]
-
 $(function(){
 	window.addEventListener('message', function(event) {
 		if (event.data.action == "setValue"){
@@ -20,48 +17,26 @@ $(function(){
 			setValue(event.data.key, event.data.value)
 		}else if (event.data.action == "updateStatus"){
 			updateStatus(event.data.status);
-		// }else if (event.data.action == "setTalking"){
-
-		// 	setTalking(event.data.value)
-		// }else if (event.data.action == "setProximity"){
-		// 	setProximity(event.data.value)
 		}else if (event.data.action == "toggle"){
 			if (event.data.show){
 				$('#ui').show();
 			} else{
 				$('#ui').hide();
 			}
-		// } else if (event.data.action == "toggleCar"){
-		// 	if (event.data.show){
-		// 		//$('.carStats').show();
-		// 	} else{
-		// 		//$('.carStats').hide();
-		// 	}
-		// }else if (event.data.action == "updateCarStatus"){
-		// 	updateCarStatus(event.data.status)
-		/*}else if (event.data.action == "updateWeight"){
-			updateWeight(event.data.weight)*/
+		}else if (event.data.action == "showcarhud"){
+			if (event.data.showhud){
+				$('.huds').fadeIn();
+				setProgressSpeed(event.data.speed,'.progress-speed');
+				setProgressFuel(event.data.fuel,'.progress-fuel');
+			} else{
+				$('.huds').fadeOut();
+			}
 		}
+	  
 	});
 
 });
 
-// function updateWeight(weight){
-
-
-// 	var bgcolor = colourGradient(weight/100, rgbEnd, rgbStart)
-// 	$('#weight .bg').css('height', weight+'%')
-// 	$('#weight .bg').css('background-color', 'rgb(' + bgcolor[0] +','+ bgcolor[1] +','+ bgcolor[2] +')')
-// }
-
-// function updateCarStatus(status){
-// 	var gas = status[0]
-// 	$('#gas .bg').css('height', gas.percent+'%')
-// 	var bgcolor = colourGradient(gas.percent/100, rgbStart, rgbEnd)
-// 	//var bgcolor = colourGradient(0.1, rgbStart, rgbEnd)
-// 	//$('#gas .bg').css('height', '10%')
-// 	$('#gas .bg').css('background-color', 'rgb(' + bgcolor[0] +','+ bgcolor[1] +','+ bgcolor[2] +')')
-// }
 
 function setValue(key, value){
 	$('#'+key+' span').html(value)
@@ -70,7 +45,6 @@ function setValue(key, value){
 
 function setJobIcon(value){
 	$('#job img').attr('src', 'img/jobs/'+value+'.png')
-	//$('#job img').attr('src', 'img/jobs/'+value+'.png')
 }
 
 function setJob2Icon(value){
@@ -99,43 +73,37 @@ function updateStatus(status){
 
 }
 
-// function setProximity(value){
-// 	var color;
-// 	var speaker;
-// 	if (value == "whisper"){
-// 		color = "#FFEB3B";
-// 		speaker = 1;
-// 	}else if (value == "normal"){
-// 		color = "#81C784"
-// 		speaker = 2;
-// 	}else if (value == "shout"){
-// 		color = "#e53935"
-// 		speaker = 3;
 
-// 	}
-// 	$('#voice .bg').css('background-color', color);
-// 	$('#voice img').attr('src', 'img/speaker'+speaker+'.png');
-// }	
+  // Fuel
+  function setProgressFuel(percent, element){
+    var circle = document.querySelector(element);
+    var radius = circle.r.baseVal.value;
+    var circumference = radius * 2 * Math.PI;
+    var html = $(element).parent().parent().find('span');
 
-// function setTalking(value){
-// 	if (value){
-// 		//#64B5F6
-// 		$('#voice').css('border', '3px solid #03A9F4')
-// 	}else{
-// 		//#81C784
-// 		$('#voice').css('border', 'none')
-// 	}
-// }
+    circle.style.strokeDasharray = `${circumference} ${circumference}`;
+    circle.style.strokeDashoffset = `${circumference}`;
 
-//API Shit
-// function colourGradient(p, rgb_beginning, rgb_end){
-//     var w = p * 2 - 1;
+    const offset = circumference - ((-percent*73)/100) / 100 * circumference;
+    circle.style.strokeDashoffset = -offset;
 
-//     var w1 = (w + 1) / 2.0;
-//     var w2 = 1 - w1;
+    html.text(Math.round(percent));
+  }
 
-//     var rgb = [parseInt(rgb_beginning[0] * w1 + rgb_end[0] * w2),
-//         parseInt(rgb_beginning[1] * w1 + rgb_end[1] * w2),
-//             parseInt(rgb_beginning[2] * w1 + rgb_end[2] * w2)];
-//     return rgb;
-// };
+  // Speed
+  function setProgressSpeed(value, element){
+    var circle = document.querySelector(element);
+    var radius = circle.r.baseVal.value;
+    var circumference = radius * 2 * Math.PI;
+    var html = $(element).parent().parent().find('span');
+    var percent = value*100/220;
+    if (percent > 100) percent = 100;
+
+    circle.style.strokeDasharray = `${circumference} ${circumference}`;
+    circle.style.strokeDashoffset = `${circumference}`;
+
+    const offset = circumference - ((-percent*73)/100) / 100 * circumference;
+    circle.style.strokeDashoffset = -offset;
+
+    html.text(value);
+  }
