bf0167bd82ad8721042ae3be5e906a9b5132661a
AvaN0x <clement.ricatte57@gmail.com>
2020-09-08 18:43:12 +0200

    Add a new command to get skin value

--- resources/[esx]/[basics]/esx_avan0x/server.lua
+++ resources/[esx]/[basics]/esx_avan0x/server.lua
@@ -20,3 +20,56 @@ AddEventHandler('esx_avan0x:logTransaction', function(identifier_origin, account
 	end)
 
 end)
+
+RegisterCommand('sendskin', function(source)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	MySQL.Async.fetchAll(
+	  'SELECT * FROM users WHERE identifier = @identifier',
+	  {
+		['@identifier'] = xPlayer.identifier
+	  },
+	  function(users)
+		local user = users[1]
+		
+		if user.skin ~= nil then
+			local skin = {}
+			for k,v in pairs(json.decode(user.skin)) do
+				if k == "tshirt_1"
+				or k == "tshirt_2"
+				or k == "torso_1"
+				or k == "torso_2"
+				or k == "pants_1"
+				or k == "pants_2"
+				or k == "shoes_1"
+				or k == "shoes_2"
+				or k == "chain_1"
+				or k == "chain_2"
+				or k == "bags_1"
+				or k == "bags_2"
+				or k == "bproof_1"
+				or k == "bproof_2"
+				or k == "helmet_1"
+				or k == "helmet_2"
+				or k == "arms"
+				or k == "glasses_1"
+				or k == "glasses_2"
+				then
+					table.insert(skin, k..":"..v)
+				end
+			end
+
+			local skinjson = "{"..table.concat(skin, ",").."}"
+			print(skinjson)
+			SendWebhookMessage("```json\n"..skinjson.."```")
+		end  
+	  end)
+
+end)
+
+function SendWebhookMessage(message)
+	webhook = GetConvar("avan0x_webhook", "none")
+	if webhook ~= "none" then
+		PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({content = message}), { ['Content-Type'] = 'application/json' })
+	end
+end
