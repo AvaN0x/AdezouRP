e71b3dc34de23e56471e008e1d648310c37c2e0b
AvaN0x <clement.ricatte57@gmail.com>
2020-09-13 18:23:23 +0200

    reorganisation of esx_avan0x

--- resources/[esx]/[basics]/esx_avan0x/__resource.lua
+++ resources/[esx]/[basics]/esx_avan0x/__resource.lua
@@ -2,9 +2,13 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 version '1.0.0'
 author 'github.com/AvaN0x'
-description 'ESX AvaN0x needs'
+description 'ESX AvaN0x -- random shit'
 
-client_script 'client.lua' --your NUI Lua File
-server_script "@mysql-async/lib/MySQL.lua"
-server_script 'server.lua'
+client_scripts {
+    'client/client.lua'
+}
 
+server_scripts {
+    "@mysql-async/lib/MySQL.lua",
+    'server/server.lua'
+}
