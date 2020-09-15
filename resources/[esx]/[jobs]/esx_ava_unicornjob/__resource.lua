19a62b79463e1c252b9e9ef673dbec9cfaae2f98
AvaN0x <clement.ricatte57@gmail.com>
2020-09-15 22:10:36 +0200

    oops I forgot ssomething

--- resources/[esx]/[jobs]/esx_ava_unicornjob/__resource.lua
+++ resources/[esx]/[jobs]/esx_ava_unicornjob/__resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 version '1.0.0'
 author 'github.com/AvaN0x'
-description 'Vigneron Job'
+description 'Unicorn Job'
 
 client_scripts {
   '@es_extended/locale.lua',
