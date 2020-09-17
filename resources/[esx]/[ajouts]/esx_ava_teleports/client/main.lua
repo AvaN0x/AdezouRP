cc21fbe8d391f1bdd50030d60f0d60873219db3d
AvaN0x <clement.ricatte57@gmail.com>
2020-09-17 18:09:07 +0200

    remove useless things

--- resources/[esx]/[ajouts]/esx_ava_teleports/client/main.lua
+++ resources/[esx]/[ajouts]/esx_ava_teleports/client/main.lua
@@ -43,54 +43,16 @@ Citizen.CreateThread(function()
 		local playerCoords = GetEntityCoords(PlayerPedId())
 
 		for k,tpID in ipairs(Config.Teleporters) do
-			local distance
-
-			distance = #(playerCoords - tpID.tpEnter.pos)
-			-- if tpID.doors then
-			-- 	distance = #(playerCoords - tpID.doors[1].objCoords)
-			-- else
-			-- 	distance = #(playerCoords - tpID.objCoords)
-			-- end
-
+			local distance = #(playerCoords - tpID.tpEnter.pos)
 			local isAuthorized = IsAuthorized(tpID)
-			local maxDistance, helpText = 2.5, _U('unlocked')
+			local maxDistance = 2.5
+			local helpText = _U('unlocked')
 
-			if tpID.distance then
-				maxDistance = tpID.distance
-			end
-
-			-- if distance < 50 then
-			-- 	if tpID.doors then
-			-- 		for _,v in ipairs(tpID.doors) do
-			-- 			FreezeEntityPosition(v.object, tpID.locked)
-
-			-- 			if tpID.locked and v.objYaw and GetEntityRotation(v.object).z ~= v.objYaw then
-			-- 				SetEntityRotation(v.object, 0.0, 0.0, v.objYaw, 2, true)
-			-- 			elseif not v.locked and v.objOpenYaw and GetEntityRotation(v.object).z ~= v.objOpenYaw then
-			-- 				FreezeEntityPosition(v.object, not tpID.locked)
-			-- 				SetEntityRotation(v.object, 0.0, 0.0, v.objYaw, 2, true)
-			-- 			end
-			-- 		end
-			-- 	else
-			-- 		FreezeEntityPosition(tpID.object, tpID.locked)
-
-			-- 		if tpID.locked and tpID.objYaw and GetEntityRotation(tpID.object).z ~= tpID.objYaw then
-			-- 			SetEntityRotation(tpID.object, 0.0, 0.0, tpID.objYaw, 2, true)
-			-- 		elseif not tpID.locked and tpID.objOpenYaw and GetEntityRotation(tpID.object).z ~= tpID.objOpenYaw then
-			-- 			FreezeEntityPosition(tpID.object, not tpID.locked)
-			-- 			SetEntityRotation(tpID.object, 0.0, 0.0, tpID.objOpenYaw, 2, true)
-			-- 		end
-			-- 	end
-			-- end
+			if tpID.distance then maxDistance = tpID.distance end
 
 			if distance < maxDistance then
-				if tpID.locked then
-					helpText = _U('locked')
-				end
-
-				if isAuthorized then
-					helpText = _U('press_button', helpText)
-				end
+				if tpID.locked then	helpText = _U('locked')	end
+				if isAuthorized then helpText = _U('press_button', helpText) end
 
 				ESX.Game.Utils.DrawText3D(tpID.tpEnter.pos, tpID.tpEnter.label, 0.5) -- draw label
 				ESX.ShowHelpNotification(helpText)
@@ -99,18 +61,10 @@ Citizen.CreateThread(function()
 					if not tpID.locked then
 						print("tp here")
 					end
-					-- if isAuthorized then
-					-- 	tpID.locked = not tpID.locked
-
-					-- 	print(k..' '..tostring(tpID.locked))
-					-- 	TriggerServerEvent('esx_ava_teleports:updateState', k, tpID.locked) -- Broadcast new state of the door to everyone
-					-- end
 				end
 				if IsControlJustReleased(0, 73) then -- X
 					if isAuthorized then
 						tpID.locked = not tpID.locked
-
-						print(k..' '..tostring(tpID.locked))
 						TriggerServerEvent('esx_ava_teleports:updateState', k, tpID.locked) -- Broadcast new state of the door to everyone
 					end
 				end
