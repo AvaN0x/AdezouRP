74eaff0b0b7d3c94955f6ccf3efd9cd828f12efe
AvaN0x <clement.ricatte57@gmail.com>
2020-09-19 17:27:10 +0200

    change blip for fuel station, and remove some useless blip

--- resources/[esx]/[car]/esx_legacyfuel/source/fuel_client.lua
+++ resources/[esx]/[car]/esx_legacyfuel/source/fuel_client.lua
@@ -349,11 +349,10 @@ function CreateBlip(coords)
 	local blip = AddBlipForCoord(coords)
 
 	SetBlipSprite(blip, 361)
-	SetBlipScale(blip, 0.6)
+	SetBlipScale(blip, 0.5)
 	SetBlipColour(blip, 4)
 	SetBlipDisplay(blip, 4)
 	SetBlipAsShortRange(blip, true)
-
 	BeginTextCommandSetBlipName("STRING")
 	AddTextComponentString("Station Service")
 	EndTextCommandSetBlipName(blip)
