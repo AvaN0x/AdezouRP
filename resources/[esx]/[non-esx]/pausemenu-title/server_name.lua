39ef46445f832ef2420f319c9fc6a2f5451f8b4d
AvaN0x <clement.ricatte57@gmail.com>
2020-09-20 16:16:51 +0200

    edit invite again

--- resources/[esx]/[non-esx]/pausemenu-title/server_name.lua
+++ resources/[esx]/[non-esx]/pausemenu-title/server_name.lua
@@ -9,7 +9,7 @@ end
 	local name = GetPlayerName(PlayerId())
 	local id = GetPlayerServerId(PlayerId())
 	--Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), 'FE_THDR_GTAO', '~y~AltisCity ~t~| ~g~Discord: w5yD9Mp~t~ | ~b~ID: ' .. id .. ' ~t~| ~b~Nom: ~b~' .. name .. " ~t~| ~r~Joueurs: " .. #players .. "/80")
-	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), 'FE_THDR_GTAO', "~r~Adezou RôlePlay~s~ | Discord : ~g~discord.gg/xUTnEVV~s~ | ID: "..id.." | ~r~".. #players .." connecté(e)s")
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), 'FE_THDR_GTAO', "~r~Adezou RôlePlay~s~ | Discord : ~g~discord.gg/3Q8dvDT~s~ | ID: "..id.." | ~r~".. #players .." connecté(e)s")
 end
 
 Citizen.CreateThread(function()
