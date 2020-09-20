dd9f6de6a0592b0c80b6b6787ff2796ba1e1051b
AvaN0x <clement.ricatte57@gmail.com>
2020-09-20 17:29:21 +0200

    Add arcade room teleports

--- resources/[esx]/[ajouts]/esx_ava_teleports/config.lua
+++ resources/[esx]/[ajouts]/esx_ava_teleports/config.lua
@@ -10,7 +10,7 @@ Config.DefaultSize = {x = 1.5, y = 1.5, z = 1.0}
 Config.DefaultColor = {r = 255, g = 255, b = 255}
 
 Config.Teleporters = {
-	{
+	{ -- hospital elevator
 		tpEnter = {
 			pos = vector3(329.78, -600.99, 42.3),
 			heading = 64.0,
@@ -27,7 +27,7 @@ Config.Teleporters = {
 		locked = true,
 		distance = 5
 	},
-	{
+	{ -- nightclub entry
 		tpEnter = {
 			pos = vector3(-676.83, -2458.72, 12.96),
 			heading = 141.0,
@@ -44,6 +44,46 @@ Config.Teleporters = {
 		locked = true,
 		distance = 5
 	},
+
+
+	{ -- arcade entry
+		tpEnter = {
+			pos = vector3(-1270.27, -305.18, 36.01),
+			heading = 250.0,
+			-- size  = {x = 1.5, y = 1.5, z = 1.0},
+			-- color = {r = 255, g = 0, b = 255},
+			label = "Entrer"
+		},
+		tpExit = {
+			pos = vector3(2737.95, -374.35, -48.97),
+			heading = 180.0,
+			label = "Sortir"
+		},
+		authorizedJobs = { '' },
+		locked = false,
+		distance = 5
+	},
+
+	{ -- arcade vehicle entry
+		tpEnter = {
+			pos = vector3(-1288.73, -275.0, 37.81),
+			heading = 30.0,
+			size  = {x = 2.5, y = 2.5, z = 1.0},
+			-- color = {r = 255, g = 0, b = 255},
+			label = "Entrer"
+		},
+		tpExit = {
+			pos = vector3(2680.36, -361.16, -56.17),
+			heading = 270.0,
+			size  = {x = 2.5, y = 2.5, z = 1.0},
+			label = "Sortir"
+		},
+		authorizedJobs = { '' },
+		allowVehicles = true,
+		locked = false,
+		distance = 5
+	},
+
 
 	-- exemple to teleport vehicle
 	-- {
