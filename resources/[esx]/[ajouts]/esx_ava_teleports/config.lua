3a9da8ad4e50277ca0ff041e581327f31f2eefdc
AvaN0x <clement.ricatte57@gmail.com>
2020-09-20 16:16:57 +0200

    add todo

--- resources/[esx]/[ajouts]/esx_ava_teleports/config.lua
+++ resources/[esx]/[ajouts]/esx_ava_teleports/config.lua
@@ -10,6 +10,7 @@ Config.DefaultSize = {x = 1.5, y = 1.5, z = 1.0}
 Config.DefaultColor = {r = 255, g = 255, b = 255}
 
 Config.Teleporters = {
+	-- TODO add the ability to hide the marker
 	{
 		tpEnter = {
 			pos = vector3(329.78, -600.99, 42.3),
