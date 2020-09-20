6df61fb56091adb08d0641b65d9c0a294accdbc8
AvaN0x <clement.ricatte57@gmail.com>
2020-09-20 18:29:38 +0200

    edit some strings

--- resources/[esx]/[ajouts]/esx_dmvschool/html/questions.js
+++ resources/[esx]/[ajouts]/esx_dmvschool/html/questions.js
@@ -1,5 +1,4 @@
-var tableauQuestion = [
-	{
+var tableauQuestion = [{
 		question: "Si vous conduisez à 80km/h, que vous approchez d'un lieu de résidence, cela veut dire que :",
 		propositionA: "Vous devez accélérer",
 		propositionB: "Vous pouvez garder votre vitesse, si vous ne croisez pas d'autres véhicules",
@@ -11,9 +10,9 @@ var tableauQuestion = [
 	{
 		question: "Vous vous apprétez à tourner à droite au feu vert, mais vous voyez un piéton qui traverse :",
 		propositionA: "Vous passez avant le piéton",
-		propositionB: "Vous vérifiez qu'il n'y a pas d'autre véhicule et vous passez",
-		propositionC: "Vous attendez que le piéton est terminé",
-		propositionD: "Vous shoutez le piéton pour passer",
+		propositionB: "Vous vérifiez qu'il n'y ait pas d'autre véhicule et vous passez",
+		propositionC: "Vous attendez que le piéton ait terminé",
+		propositionD: "Vous écrasez le piéton pour passer",
 		reponse: "C"
 	},
 
@@ -21,8 +20,8 @@ var tableauQuestion = [
 		question: "Sans aucune indication : La vitesse dans une zone résidentielle est de : __ km/h",
 		propositionA: "50",
 		propositionB: "55",
-		propositionC: "65",
-		propositionD: "70",
+		propositionC: "70",
+		propositionD: "90",
 		reponse: "A"
 	},
 
@@ -45,10 +44,10 @@ var tableauQuestion = [
 	},
 
 	{
-		question: "A quel moment vous pouvez passer aux feux ?",
+		question: "A quel moment pouvez-vous passer aux feux ?",
 		propositionA: "Quand il est vert",
 		propositionB: "Quand il n'y a personne sur l'intersection",
-		propositionC: "Vous êtes dans une zone d'école",
+		propositionC: "Quand il est rouge",
 		propositionD: "Quand il est vert et/ou rouge et que je tourne à droite",
 		reponse: "D"
 	},
@@ -63,21 +62,21 @@ var tableauQuestion = [
 	},
 
 	{
-		question: "Qu'est ce qui est permit quand vous dépassez un autre véhicule",
+		question: "Qu'est ce qui est permit lorsque vous dépassez un autre véhicule",
 		propositionA: "Le suivre de près pour le doubler plus vite",
 		propositionB: "Le doubler en quittant la route",
-		propositionC: "Conduire sur la route opposé pour le dépasser",
+		propositionC: "Conduire sur la route opposée pour le dépasser",
 		propositionD: "Dépasser la vitesse limite",
 		reponse: "C"
 	},
 
 	{
-		question: "Vous conduisez sur une rocade qui indique une vitesse maximum de 110 km/h. La plupart du traffic roule à 120 km/h, alors vous ne devriez pas conduire plus vite que :",
-		propositionA: "80 kmh",
-		propositionB: "40 kmh",
-		propositionC: "50 kmh",
-		propositionD: "110 kmh",
-		reponse: "D"
+		question: "Vous conduisez sur une rocade qui indique une vitesse maximum de 130 km/h. La plupart du traffic roule à 140 km/h, alors vous ne devriez pas conduire plus vite que :",
+		propositionA: "50 kmh",
+		propositionB: "90 kmh",
+		propositionC: "130 kmh",
+		propositionD: "140 kmh",
+		reponse: "C"
 	},
 
 	{
