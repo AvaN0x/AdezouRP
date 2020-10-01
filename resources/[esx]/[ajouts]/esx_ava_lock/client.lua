595538ee595748b5e314ac18e1a5a3af8325b75a
AvaN0x <clement.ricatte57@gmail.com>
2020-10-01 14:44:25 +0200

    animation when locking/unlocking a door / tp cannot be played anymore in a vehicle

--- resources/[esx]/[ajouts]/esx_ava_lock/client.lua
+++ resources/[esx]/[ajouts]/esx_ava_lock/client.lua
@@ -4,10 +4,13 @@
 -------------------------------------------
 RegisterNetEvent('esx_ava_lock:dooranim')
 AddEventHandler('esx_ava_lock:dooranim', function()
-    ESX.Streaming.RequestAnimDict("anim@heists@keycard@", function()
-        TaskPlayAnim(GetPlayerPed(-1), "anim@heists@keycard@", "exit", 8.0, -8, 1200, 0, 0, 0, 0, 0)
+    local playerPed = GetPlayerPed(-1)
+    if not IsPedSittingInAnyVehicle(playerPed) then
+        ESX.Streaming.RequestAnimDict("anim@heists@keycard@", function()
+            TaskPlayAnim(playerPed, "anim@heists@keycard@", "exit", 8.0, -8, 1200, 0, 0, 0, 0, 0)
 
-        Citizen.Wait(1200)
-        ClearPedSecondaryTask(GetPlayerPed(-1))
-    end)
+            Citizen.Wait(1200)
+            ClearPedSecondaryTask(playerPed)
+        end)
+    end
 end)
