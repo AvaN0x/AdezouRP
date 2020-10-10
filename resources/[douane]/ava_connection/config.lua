5d2c9971ec4244784267e8978e9fa14a92f5e597
AvaN0x <clement.ricatte57@gmail.com>
2020-10-10 15:54:12 +0200

    add connection managment in ava_connection

--- resources/[douane]/ava_connection/config.lua
+++ resources/[douane]/ava_connection/config.lua
@@ -1,4 +1,8 @@
 Config = {
 	GuildId = "743525702157992018",
 
+	-- you just need one of them
+	WhitelistedRoles = {
+		"743525702531547228" -- Citoyen
+	}
 }
--- resources/[douane]/ava_connection/server.lua
+++ resources/[douane]/ava_connection/server.lua
@@ -55,7 +55,54 @@ Citizen.CreateThread(function()
 end)
 
 
+function has_value(table, val)
+	if table then
+		for k, v in ipairs(table) do
+			if v == val then
+				return true
+			end
+		end
+	end
+	return false
+end
 
+AddEventHandler("playerConnecting", function(name, setCallback, deferrals)
+    local gotRole = false
+	local discordId = nil
+	deferrals.defer()
+	deferrals.update("Vérification des permissions...")
+
+    for k, v in ipairs(GetPlayerIdentifiers(source)) do
+        if string.sub(v, 1, string.len("discord:")) == "discord:" then
+			discordId = string.gsub(v, "discord:", "")
+            deferrals.update("Ton ID discord a été trouvé...")
+            break
+        end
+    end
+
+    if discordId then
+		local endpoint = ("guilds/%s/members/%s"):format(Config.GuildId, discordId)
+		local member = DiscordRequest("GET", endpoint, {})
+		if member.code == 200 then
+			local data = json.decode(member.data)
+			for k, v in ipairs(Config.WhitelistedRoles) do 
+				if has_value(data.roles, v) then
+					gotRole = true
+					break
+				end
+			end
+
+			if gotRole then
+				SendWebhookEmbedMessage("avan0x_wh_connections", "", data.user.username.."#"..data.user.discriminator .. " se connecte.", 311891)
+				deferrals.done()
+				return
+			end
+		end
+		deferrals.done("Pour pouvoir rejoindre le serveur, connectez-vous au discord (discord.gg/KRTKC6b)")
+    else
+        deferrals.done("Discord n'a pas été détecté. Veuillez vous assurer que Discord est en cours d'exécution et est installé. Voir le lien ci-dessous pour un processus de débogage - docs.faxes.zone/docs/debugging-discord")
+    end
+end)
 
 AddEventHandler('playerDropped', function(reason)
     for k, v in ipairs(GetPlayerIdentifiers(source)) do
@@ -70,13 +117,13 @@ AddEventHandler('playerDropped', function(reason)
         local member = DiscordRequest("GET", endpoint, {})
         if member.code == 200 then
 			local data = json.decode(member.data)
-			print(data.username.."#"..data.discriminator .. " left.\n\tReason : "..reason)
-			SendWebhookEmbedMessage("avan0x_wh_connections", "", data.username.."#"..data.discriminator .. " left.\n\tReason : "..reason, 16733269)
+			print(data.username.."#"..data.discriminator .. " a quitté.\n\tRaison : "..reason)
+			SendWebhookEmbedMessage("avan0x_wh_connections", "", data.username.."#"..data.discriminator .. " a quitté.\n\tRaison : "..reason, 16733269)
 			return
 		end
 	end
-	print((GetPlayerName(source) or "An user") .. " left.\n\tReason : "..reason)
-	SendWebhookEmbedMessage("avan0x_wh_connections", "", (GetPlayerName(source) or "An user") .. " left.\n\tReason : "..reason, 16733269)
+	print((GetPlayerName(source) or "An user") .. " a quitté.\n\tRaison : "..reason)
+	SendWebhookEmbedMessage("avan0x_wh_connections", "", (GetPlayerName(source) or "An user") .. " a quitté.\n\tRaison : "..reason, 16733269)
 end)
 
 
