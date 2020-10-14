ac8489a79fb31c7a88f9af9b5ffca398106a7bf0
AvaN0x <clement.ricatte57@gmail.com>
2020-10-14 11:01:08 +0200

    edit __resource.lua of new_banking

--- resources/[esx]/[basics]/new_banking/__resource.lua
+++ resources/[esx]/[basics]/new_banking/__resource.lua
@@ -1,6 +1,11 @@
-client_script('client/client.lua') --your NUI Lua File
+version '1.0.0'
+author 'github.com/AvaN0x'
+description 'new_banking edited by AvaN0x'
+
 server_script "@mysql-async/lib/MySQL.lua"
 server_script 'server.lua'
+
+client_script('client/client.lua') --your NUI Lua File
 ui_page('client/html/UI.html') --THIS IS IMPORTENT
 
 --[[The following is for the files which are need for you UI (like, pictures, the HTML file, css and so on) ]]--
