79bacab94e53b1ecdc9fb9d52a48fcc672e236de
AvaN0x <clement.ricatte57@gmail.com>
2020-10-16 18:56:12 +0200

    death support, add text when sitting and things

--- resources/[esx]/[ajouts]/esx_ava_chairs/client.lua
+++ resources/[esx]/[ajouts]/esx_ava_chairs/client.lua
@@ -157,25 +157,12 @@ end
 
 local chair = nil
 local isSitting = false
-
+local IsDead = false
 local oldCoords = nil
-
-local Props = {
-	{hash = -1531508740, offX = 0.0, offY = 0.0, offZ = -1.0, offHeading = 180.0, bed = false},
-
-    {hash = GetHashKey("v_med_bed1"), offX = 0.0, offY = 0.0, offZ = -1.4, offHeading = 0.0, bed = true},
-    {hash = GetHashKey("v_med_bed2"), offX = 0.0, offY = 0.0, offZ = -1.4, offHeading = 0.0, bed = true},
-    {hash = GetHashKey("v_med_emptybed"), offX = 0.0, offY = 0.0, offZ = -1.2, offHeading = 90.0, bed = true},
-	{hash = GetHashKey("prop_bench_01a"), offX = 0.0, offY = 0.0, offZ = -0.4, offHeading = 180.0, bed = false},
-	{hash = GetHashKey("prop_busstop_02"), offX = 0.0, offY = 0.5, offZ = -0.4, offHeading = 180.0, bed = false},
-
-}
-
-
 
 Citizen.CreateThread(function()
 	while true do
-		Wait(500)
+		Wait(1000)
         chair = getChair()
 	end
 end)
@@ -183,26 +170,20 @@ end)
 Citizen.CreateThread(function()
 	while true do
         Wait(0)
-        if chair and not isSitting then
-            DrawMarker(27, chair.x, chair.y, chair.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 1.5, 1.5, 1.5, 255, 255, 0, 100, false, true, 2, false, false, false, false)
+        if not IsDead then
+            if chair and not isSitting then
+                DrawText3D(chair.x, chair.y, chair.z, _U("sit_down"))
 
-            if IsControlJustPressed(0, 38) then -- E
-				local playerPed = PlayerPedId()
-				isSitting = true
-				oldCoords = GetEntityCoords(playerPed)
+                if IsControlJustPressed(0, Config.Key) then -- E
+                    Animation(Config.Anims[chair.type])
+                end
+            elseif chair and isSitting then
+                DrawText3D(chair.x, chair.y, chair.z, _U("stand_up"))
 
-                    TaskStartScenarioAtPosition(playerPed, Config.objects.SitAnimation.anim, chair.x, chair.y, chair.z, chair.heading, 0, true, true)
-			end
-		elseif chair and isSitting then
-			DrawMarker(27, chair.x, chair.y, chair.z, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 1.5, 1.5, 1.5, 255, 0, 0, 100, false, true, 2, false, false, false, false)
-
-			if IsControlJustPressed(0, 38) then -- E
-				local playerPed = PlayerPedId()
-				isSitting = false
-
-				SetEntityCoords(playerPed, oldCoords.x, oldCoords.y, oldCoords.z - 0.98)
-			end
-
+                if IsControlJustPressed(0, Config.Key) then -- E
+                    StandUp()
+                end
+            end
         end
     end
 end)
@@ -211,16 +192,74 @@ end)
 function getChair()
 	local playerPed = PlayerPedId()
 	local coords = GetEntityCoords(playerPed)
-	for _,v in ipairs(Props) do
-		local closestProp = GetClosestObjectOfType(coords, 1.0, v.hash, false, false, false)
+	for _,v in ipairs(Config.Props) do
+		local closestProp = GetClosestObjectOfType(coords, 0.7, v.hash, false, false, false)
 
 		if DoesEntityExist(closestProp) then
 			local markerCoords = GetOffsetFromEntityInWorldCoords(closestProp, v.offX, v.offY, v.offZ)
 
-			return {x = markerCoords.x, y = markerCoords.y, z = markerCoords.z + 0.9, heading = GetEntityHeading(closestProp) + v.offHeading, bed = v.bed}
+			return {x = markerCoords.x, y = markerCoords.y, z = markerCoords.z + 0.9, heading = GetEntityHeading(closestProp) + v.offHeading, type = v.type}
 		end
 	end
 	return nil
 end
 
 
+function DrawText3D(x, y, z, text)
+    local onScreen, _x, _y = World3dToScreen2d(x, y, z)
+
+    if onScreen then
+        SetTextScale(0.35, 0.35)
+        SetTextFont(4)
+        SetTextProportional(1)
+        SetTextColour(255, 255, 255, 215)
+        SetTextEntry("STRING")
+        SetTextCentre(1)
+        SetTextOutline()
+
+        AddTextComponentString(text)
+        DrawText(_x, _y)
+    end
+end
+
+function Animation(anim)
+    isSitting = true
+    local playerPed = PlayerPedId()
+    oldCoords = GetEntityCoords(playerPed)
+
+    TriggerEvent("skinchanger:getSkin", function(skin)
+        local anim = anim[skin.sex and "Female" or "Male"]
+        if anim.dict ~= nil then
+            SetEntityCoords(playerPed, chair.x, chair.y, chair.z)
+            SetEntityHeading(playerPed, chair.heading)
+
+            RequestAnimDict(anim.dict)
+            while not HasAnimDictLoaded(anim.dict) do
+                Citizen.Wait(0)
+            end
+
+            TaskPlayAnim(playerPed, anim.dict, anim.anim, 8.0, 1.0, -1, 1, 0, 0, 0, 0)
+            FreezeEntityPosition(playerPed, true)
+        else
+            TaskStartScenarioAtPosition(playerPed, anim.scenario, chair.x, chair.y, chair.z, chair.heading, 0, true, true)
+        end
+    end)
+end
+
+function StandUp()
+    isSitting = false
+    local playerPed = PlayerPedId()
+
+    ClearPedTasksImmediately(playerPed)
+    FreezeEntityPosition(playerPed, false)
+    SetEntityCoords(playerPed, oldCoords.x, oldCoords.y, oldCoords.z - 0.98)
+end
+
+AddEventHandler("esx:onPlayerDeath", function()
+    StandUp()
+	IsDead = true
+end)
+
+AddEventHandler("playerSpawned", function(spawn)
+	IsDead = false
+end)
--- resources/[esx]/[ajouts]/esx_ava_chairs/config.lua
+++ resources/[esx]/[ajouts]/esx_ava_chairs/config.lua
@@ -4,7 +4,44 @@
 -------------------------------------------
 
 Config = {}
+Config.Locale = "fr"
 
-Config.objects = {
-	SitAnimation = {anim='PROP_HUMAN_SEAT_CHAIR_MP_PLAYER'},
+Config.Key = 38
+
+Config.Props = {
+	{hash = -1531508740, offX = 0.0, offY = 0.0, offZ = -1.0, offHeading = 180.0, type = "Chair"},
+	{hash = GetHashKey("v_med_bed1"), offX = 0.0, offY = 0.0, offZ = -0.8, offHeading = 180.0, type = "Bed"},
+	{hash = GetHashKey("v_med_bed2"), offX = 0.0, offY = 0.0, offZ = -0.8, offHeading = 0.0, type = "Bed"},
+	{hash = GetHashKey("prop_bench_01a"), offX = 0.0, offY = 0.0, offZ = -0.4, offHeading = 180.0, type = "Chair"},
+	{hash = GetHashKey("prop_busstop_02"), offX = 0.0, offY = 0.5, offZ = -0.4, offHeading = 180.0, type = "Chair"},
+	{hash = GetHashKey("v_corp_offchair"), offX = 0.0, offY = 0.0, offZ = -0.4, offHeading = 180.0, type = "Chair"},
+
 }
+
+Config.Anims = {
+	Chair = {
+		Male = {
+			scenario = "PROP_HUMAN_SEAT_CHAIR_MP_PLAYER",
+			-- dict = ,
+			-- anim = 
+		},
+		Female = {
+			scenario = "PROP_HUMAN_SEAT_CHAIR_MP_PLAYER",
+			-- dict = ,
+			-- anim = 
+		}
+	},
+	Bed = {
+		Male = {
+			dict = "switch@trevor@annoys_sunbathers",
+			anim = "trev_annoys_sunbathers_loop_guy"
+		},
+		Female = {
+			dict = "switch@trevor@annoys_sunbathers",
+			anim = "trev_annoys_sunbathers_loop_guy"
+		}
+	}
+}
+
+-- dict = "amb@world_human_sunbathe@female@front@base",
+-- anim = "base"
