f072430aa21db08ac95eaac61f8ec3bf4c13fbfc
AvaN0x <clement.ricatte57@gmail.com>
2020-10-17 17:39:30 +0200

    ragdoll when hit in legs is not in esx_avan0x

--- resources/[esx]/[basics]/esx_avan0x/client/weapons_cl.lua
+++ resources/[esx]/[basics]/esx_avan0x/client/weapons_cl.lua
@@ -137,3 +137,53 @@ end)
 --         SetPedSuffersCriticalHits(PlayerPedId(), false) -- headshot will not be a critical hit
 --     end
 -- end)
+
+
+
+--============================--
+--= Ragdoll when hit in legs =--
+--============================--
+
+local Bones = {
+	--[[Pelvis]][11816] = true,
+	--[[SKEL_L_Thigh]][58271] = true,
+	--[[SKEL_L_Calf]][63931] = true,
+	--[[SKEL_L_Foot]][14201] = true,
+	--[[SKEL_L_Toe0]][2108] = true,
+	--[[IK_L_Foot]][65245] = true,
+	--[[PH_L_Foot]][57717] = true,
+	--[[MH_L_Knee]][46078] = true,
+	--[[SKEL_R_Thigh]][51826] = true,
+	--[[SKEL_R_Calf]][36864] = true,
+	--[[SKEL_R_Foot]][52301] = true,
+	--[[SKEL_R_Toe0]][20781] = true,
+	--[[IK_R_Foot]][35502] = true,
+	--[[PH_R_Foot]][24806] = true,
+	--[[MH_R_Knee]][16335] = true,
+	--[[RB_L_ThighRoll]][23639] = true,
+	--[[RB_R_ThighRoll]][6442] = true,
+}
+
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		local playerPed = GetPlayerPed(-1)
+		if HasEntityBeenDamagedByAnyPed(playerPed) then
+			Ragdoll(playerPed)
+		end
+		ClearEntityLastDamageEntity(playerPed)
+	end
+end)
+
+function Ragdoll(playerPed)
+	if IsEntityDead(playerPed) then return false end
+	local hit, bone = GetPedLastDamageBone(playerPed)
+	if hit then
+		if Bones[bone] then
+			SetPedToRagdoll(playerPed, 5000, 5000, 0, 0, 0, 0)
+			return true
+		end
+	end
+	return false
+end
