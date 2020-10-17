e55f633cfe8ecca4f194e66b1f5f410b3658c4bd
AvaN0x <clement.ricatte57@gmail.com>
2020-10-17 18:01:11 +0200

    re-enable vehicles

--- resources/[esx]/[non-esx]/scriptpnj/client.lua
+++ resources/[esx]/[non-esx]/scriptpnj/client.lua
@@ -4,11 +4,13 @@ Citizen.CreateThread(function()
 		SetPedDensityMultiplierThisFrame(0.5) -- set npc/ai peds density to 0
 		SetRandomVehicleDensityMultiplierThisFrame(0.3) -- 
 		SetParkedVehicleDensityMultiplierThisFrame(0.3)
+
 		-- These natives have to be called every frame.
-		SetVehicleModelIsSuppressed(GetHashKey("rubble"), true)
-        SetVehicleModelIsSuppressed(GetHashKey("taco"), true)
-        SetVehicleModelIsSuppressed(GetHashKey("biff"), true)
-		SetGarbageTrucks(false) -- Stop garbage trucks from randomly spawning
+
+		-- SetVehicleModelIsSuppressed(GetHashKey("rubble"), true)
+        -- SetVehicleModelIsSuppressed(GetHashKey("taco"), true)
+        -- SetVehicleModelIsSuppressed(GetHashKey("biff"), true)
+		-- SetGarbageTrucks(false) -- Stop garbage trucks from randomly spawning
 		--SetRandomBoats(false) -- Stop random boats from spawning in the water.
 		SetCreateRandomCops(false) -- disable random cops walking/driving around.
 		SetCreateRandomCopsNotOnScenarios(false) -- stop random cops (not in a scenario) from spawning.
