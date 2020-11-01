41b8752abed50ca5b9dde8f86d8e622f7d855512
AvaN0x <clement.ricatte57@gmail.com>
2020-11-01 15:08:13 +0100

    change lockpick price

--- resources/[esx]/[illegal]/criminel/config.lua
+++ resources/[esx]/[illegal]/criminel/config.lua
@@ -4,6 +4,6 @@ Config.Size         = {x = 1.5, y = 1.5, z = 1.5}
 Config.Color        = {r = 0, g = 255, b = 42}
 Config.Type         = -1
 
-Config.PriceLockpick = 5000
+Config.PriceLockpick = 2000
 Config.PriceHeadBag = 2000
 Config.PriceTenueCasa = 5000
