e3584e4f716d93af7b6fd72b1b380e4784205671
AvaN0x <clement.ricatte57@gmail.com>
2020-11-05 16:43:32 +0100

    optimizations

--- resources/[esx]/[non-esx]/ProtectPnj/__resource.lua
+++ resources/[esx]/[non-esx]/ProtectPnj/__resource.lua
@@ -1,14 +1,11 @@
 
 
 client_script {
-	'deletearmevehiculeclient.lua',
 	'NoMoreCopNPC.lua',
 	'NoMoreWeaponsOnNPC.lua',
 	'NoWeaponReward.lua',
 	'DamageReducer.lua'
 }
-
-server_script 'deletearmevehiculeserver.lua'
 
 ---------------------------------
 --- Copyright by ikNox#6088 ---
