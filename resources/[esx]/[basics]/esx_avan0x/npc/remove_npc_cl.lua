fea8d14a37d98ac2a15a8cb831b05a79083a5a96
AvaN0x <clement.ricatte57@gmail.com>
2020-11-08 13:11:01 +0100

    few scripts optimization

---
+++ resources/[esx]/[basics]/esx_avan0x/npc/remove_npc_cl.lua
@@ -0,0 +1,17 @@
+Citizen.CreateThread(function()
+	while true do
+        Citizen.Wait(0)
+        --? disabled for the moment to test if it's worth it
+		-- SetPedDensityMultiplierThisFrame(0.5)
+		-- SetRandomVehicleDensityMultiplierThisFrame(0.3)
+		-- SetParkedVehicleDensityMultiplierThisFrame(0.3)
+
+		--? These natives have to be called every frame.
+        -- SetVehicleModelIsSuppressed(GetHashKey("taco"), true)
+		-- SetGarbageTrucks(false)
+		-- SetRandomBoats(false)
+		SetCreateRandomCops(false)
+		SetCreateRandomCopsNotOnScenarios(false)
+		SetCreateRandomCopsOnScenarios(false)
+	end
+end)
