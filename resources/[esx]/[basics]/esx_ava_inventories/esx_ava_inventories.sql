44c734632c174481a22c13cd7de565df15a20c1a
AvaN0x <clement.ricatte57@gmail.com>
2020-11-30 23:41:21 +0100

    edit sql values

--- resources/[esx]/[basics]/esx_ava_inventories/esx_ava_inventories.sql
+++ resources/[esx]/[basics]/esx_ava_inventories/esx_ava_inventories.sql
@@ -6,33 +6,33 @@
 CREATE TABLE IF NOT EXISTS `inventories` (
   `name` varchar(50) NOT NULL,
   `label` varchar(255) NOT NULL,
-  `max_weight` int(11) NOT NULL DEFAULT '100000',
+  `max_weight` int(11) NOT NULL DEFAULT '200000',
   `shared` tinyint(1) NOT NULL DEFAULT '0',
 
   PRIMARY KEY (`name`)
 );
 
 INSERT INTO `inventories` (`name`, `label`, `max_weight`, `shared`) VALUES
-('gang_ballas', 'Ballas', 100000, 1),
-('gang_families', 'Families', 100000, 1),
-('gang_vagos', 'Vagos', 100000, 1),
-('property', 'Propriété', 100000, 0),
+('gang_ballas', 'Ballas', 200000, 1),
+('gang_families', 'Families', 200000, 1),
+('gang_vagos', 'Vagos', 200000, 1),
+('property', 'Propriété', 200000, 0),
 ('inventory', 'Inventaire', 20000, 0),
-('society_ammunation', 'Ammunation', 100000, 1),
-('society_bahama', 'Bahamas', 100000, 1),
-('society_cluckin', 'Cluckin', 100000, 1),
-('society_ems', 'EMS', 100000, 1),
-('society_mecano', 'Mecano', 100000, 1),
-('society_nightclub', 'Galaxy', 100000, 1),
-('society_police', 'LSPD', 100000, 1),
-('society_state', 'Gouv', 100000, 1),
-('society_tailor', 'Couturier', 100000, 1),
-('society_taxi', 'Taxi', 100000, 1),
-('society_unicorn', 'Unicorn', 100000, 1),
-('society_vigneron', 'Vigneron', 100000, 1),
-('orga_celtic', 'Celtic', 100000, 1),
-('orga_ordre', 'L\'ordre', 100000, 1),
-('biker_lost', 'The Lost', 100000, 1);
+('society_ammunation', 'Ammunation', 200000, 1),
+('society_bahama', 'Bahamas', 200000, 1),
+('society_cluckin', 'Cluckin', 200000, 1),
+('society_ems', 'EMS', 200000, 1),
+('society_mecano', 'Mecano', 200000, 1),
+('society_nightclub', 'Galaxy', 200000, 1),
+('society_police', 'LSPD', 200000, 1),
+('society_state', 'Gouv', 200000, 1),
+('society_tailor', 'Couturier', 200000, 1),
+('society_taxi', 'Taxi', 200000, 1),
+('society_unicorn', 'Unicorn', 200000, 1),
+('society_vigneron', 'Vigneron', 200000, 1),
+('orga_celtic', 'Celtic', 200000, 1),
+('orga_ordre', 'L\'ordre', 200000, 1),
+('biker_lost', 'The Lost', 200000, 1);
 
 CREATE TABLE IF NOT EXISTS `inventories_items` (
   `name` varchar(50) NOT NULL,
