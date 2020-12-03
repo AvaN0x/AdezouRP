ec0f984bb679dd085ef1cde335151bd15a89a656
AvaN0x <clement.ricatte57@gmail.com>
2020-12-03 20:57:35 +0100

    now drowing a box around the entity when show_hashes is active

--- resources/[esx]/[basics]/esx_ava_personalmenu/client/admin.lua
+++ resources/[esx]/[basics]/esx_ava_personalmenu/client/admin.lua
@@ -302,6 +302,7 @@ function AdminLoop()
 			while true do
 				Citizen.Wait(3000)
 				if show_hashes then
+					-- todo choose which one to detect
 					local playerPed = PlayerPedId()
 					local playerCoords = GetEntityCoords(playerPed)
 					visibleHash = {}
@@ -309,21 +310,21 @@ function AdminLoop()
 						local prop = GetObjectIndexFromEntityIndex(v)
 						local propCoords = GetEntityCoords(prop)
 						if GetDistanceBetweenCoords(playerCoords.x, playerCoords.y, playerCoords.z, propCoords.x, propCoords.y, propCoords.z, false) < 5.0 then
-							table.insert(visibleHash, {hash = GetEntityModel(prop), coords = propCoords, heading = GetEntityHeading(prop)})
+							table.insert(visibleHash, {hash = GetEntityModel(prop), entity = prop})
 						end
 					end
 					for _, v in ipairs(GetGamePool("CVehicle")) do
-						local prop = GetObjectIndexFromEntityIndex(v)
-						local propCoords = GetEntityCoords(prop)
-						if GetDistanceBetweenCoords(playerCoords.x, playerCoords.y, playerCoords.z, propCoords.x, propCoords.y, propCoords.z, false) < 10.0 then
-							table.insert(visibleHash, {hash = GetEntityModel(prop), entity = prop})
+						local veh = GetObjectIndexFromEntityIndex(v)
+						local vehCoords = GetEntityCoords(veh)
+						if GetDistanceBetweenCoords(playerCoords.x, playerCoords.y, playerCoords.z, vehCoords.x, vehCoords.y, vehCoords.z, false) < 10.0 then
+							table.insert(visibleHash, {hash = GetEntityModel(veh), entity = veh})
 						end
 					end
 					for _, v in ipairs(GetGamePool("CPed")) do
-						local prop = GetObjectIndexFromEntityIndex(v)
-						local propCoords = GetEntityCoords(prop)
-						if GetDistanceBetweenCoords(playerCoords.x, playerCoords.y, playerCoords.z, propCoords.x, propCoords.y, propCoords.z, false) < 10.0 then
-							table.insert(visibleHash, {hash = GetEntityModel(prop), entity = prop})
+						local ped = GetObjectIndexFromEntityIndex(v)
+						local pedCoords = GetEntityCoords(ped)
+						if GetDistanceBetweenCoords(playerCoords.x, playerCoords.y, playerCoords.z, pedCoords.x, pedCoords.y, pedCoords.z, false) < 10.0 then
+							table.insert(visibleHash, {hash = GetEntityModel(ped), entity = ped})
 						end
 					end
 				else
@@ -338,12 +339,65 @@ function AdminLoop()
 				if show_hashes then
 					local playerPed = PlayerPedId()
 					for _, prop in ipairs(visibleHash) do
-						if prop.coords then
-							DrawText3D(prop.coords.x, prop.coords.y, prop.coords.z, "hash = " .. prop.hash .. '\nheading = ' .. prop.heading, 0.3)
-						else
-							local coords = GetEntityCoords(prop.entity)
-							DrawText3D(coords.x, coords.y, coords.z, prop.hash, 0.3)
-						end
+						local coords = GetEntityCoords(prop.entity)
+						DrawText3D(coords.x, coords.y, coords.z, prop.hash, 0.3)
+
+						local min, max = GetModelDimensions(GetEntityModel(prop.entity))
+						local pad = 0.001;
+
+						-- Bottom
+						local bottom1 = GetOffsetFromEntityInWorldCoords(prop.entity, min.x - pad, min.y - pad, min.z - pad)
+						local bottom2 = GetOffsetFromEntityInWorldCoords(prop.entity, max.x + pad, min.y - pad, min.z - pad)
+						local bottom3 = GetOffsetFromEntityInWorldCoords(prop.entity, max.x + pad, max.y + pad, min.z - pad)
+						local bottom4 = GetOffsetFromEntityInWorldCoords(prop.entity, min.x - pad, max.y + pad, min.z - pad)
+
+						-- Top
+						local top1 = GetOffsetFromEntityInWorldCoords(prop.entity, min.x - pad, min.y - pad, max.z + pad)
+						local top2 = GetOffsetFromEntityInWorldCoords(prop.entity, max.x + pad, min.y - pad, max.z + pad)
+						local top3 = GetOffsetFromEntityInWorldCoords(prop.entity, max.x + pad, max.y + pad, max.z + pad)
+						local top4 = GetOffsetFromEntityInWorldCoords(prop.entity, min.x - pad, max.y + pad, max.z + pad)
+
+						-- bottom
+						DrawLine(bottom1, bottom2, 255, 0, 255, 255)
+						DrawLine(bottom1, bottom4, 255, 0, 255, 255)
+						DrawLine(bottom2, bottom3, 255, 0, 255, 255)
+						DrawLine(bottom3, bottom4, 255, 0, 255, 255)
+
+						-- top
+						DrawLine(top1, top2, 255, 0, 255, 255)
+						DrawLine(top1, top4, 255, 0, 255, 255)
+						DrawLine(top2, top3, 255, 0, 255, 255)
+						DrawLine(top3, top4, 255, 0, 255, 255)
+
+						-- bottom to top
+						DrawLine(bottom1, top1, 255, 0, 255, 255)
+						DrawLine(bottom2, top2, 255, 0, 255, 255)
+						DrawLine(bottom3, top3, 255, 0, 255, 255)
+						DrawLine(bottom4, top4, 255, 0, 255, 255)
+
+						-- top face
+						DrawPoly(top1, top2, top3, 255, 0, 255, 128)
+						DrawPoly(top4, top1, top3, 255, 0, 255, 128)
+
+						-- bottom face
+						DrawPoly(bottom2, bottom1, bottom3, 255, 0, 255, 128)
+						DrawPoly(bottom1, bottom4, bottom3, 255, 0, 255, 128)
+
+						-- back face
+						DrawPoly(top2, top1, bottom1, 255, 0, 255, 128)
+						DrawPoly(bottom1, bottom2, top2, 255, 0, 255, 128)
+
+						-- front face
+						DrawPoly(top4, top3, bottom4, 255, 0, 255, 128)
+						DrawPoly(bottom3, bottom4, top3, 255, 0, 255, 128)
+
+						-- right face
+						DrawPoly(top3, top2, bottom2, 255, 0, 255, 128)
+						DrawPoly(bottom2, bottom3, top3, 255, 0, 255, 128)
+
+						-- left face
+						DrawPoly(top1, top4, bottom1, 255, 0, 255, 128)
+						DrawPoly(bottom4, bottom1, top4, 255, 0, 255, 128)
 					end
 				else
 					Citizen.Wait(5000)
@@ -402,6 +456,11 @@ function AdminLoop()
 		end)
 	end
 end
+
+
+function DrawEntityBox(entity)
+end
+
 
 function getPosition(playerPed)
 	local x, y, z = table.unpack(GetEntityCoords(playerPed, true))
