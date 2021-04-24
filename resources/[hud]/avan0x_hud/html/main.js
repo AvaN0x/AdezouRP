0121f4f8b07dfc08408a024b179dd0c6f31694c6
AvaN0x <clement.ricatte57@gmail.com>
2021-04-24 17:04:54 +0200

    fix hud

--- resources/[hud]/avan0x_hud/html/main.js
+++ resources/[hud]/avan0x_hud/html/main.js
@@ -1,152 +1,149 @@
 ﻿
 $(function () {
-	$("#job2").hide();
-	$("#gang").hide();
-	$('.mainStats').hide();
-	window.addEventListener('message', function (event) {
-		if (event.data.action == "setValue") {
-			if (event.data.key == "job") {
-				setJobIcon(event.data.name)
-			} else if (event.data.key == "job2") {
-				setJob2Icon(event.data.name);
-				if (event.data.name == "unemployed2")
-					$("#job2").hide();
-				else
-					$("#job2").show();
-			} else if (event.data.key == "gang") {
-				if (event.data.value != null) {
-					setGangIcon(event.data.name);
-					$("#gang").show();
-				} else
-					$("#gang").hide();
-			}
-			setValue(event.data.key, event.data.value);
-		} else if (event.data.action == "updateStatus") {
-			updateStatus(event.data.status);
-		} else if (event.data.action == "toggle") {
-			if (event.data.show)
-				$('#ui').show();
-			else
-				$('#ui').hide();
-		} else if (event.data.action == "togglePlayerStats") {
-			if (event.data.show) {
-				$('#playerStats').show();
-				$('.mainStats').show();
-			} else {
-				$('#playerStats').hide();
-				$('.mainStats').hide();
-			}
-		} else if (event.data.action == "toggleMainStats") {
-			if (event.data.show)
-				$('.mainStats').show();
-			else
-				$('.mainStats').hide();
-		} else if (event.data.action == "showcarhud") {
-			if (event.data.showhud) {
-				$('.huds').fadeIn();
-				setProgressSpeed(event.data.speed, '.progress-speed');
-				setProgressFuel(event.data.fuel, '.progress-fuel');
-			} else {
-				$('.huds').fadeOut();
-			}
-		} else if (event.data.action == "setbelt") {
-			if (!event.data.isAccepted || event.data.belt) {
-				$('.belt').fadeOut();
-			} else {
-				$('.belt').fadeIn();
-			}
-		} else if (event.data.action == "copyToClipboard") {
-			let copyTextarea = document.createElement('textarea');
-			let currentSelection = document.getSelection();
+    $("#job2").hide();
+    $("#gang").hide();
+    $('.mainStats').hide();
+    window.addEventListener('message', function (event) {
+        if (event.data.action == "setValue") {
+            if (event.data.key == "job") {
+                setJobIcon(event.data.name)
+            } else if (event.data.key == "job2") {
+                setJob2Icon(event.data.name);
+                if (event.data.name == "unemployed2")
+                    $("#job2").hide();
+                else
+                    $("#job2").show();
+            } else if (event.data.key == "gang") {
+                if (event.data.value != null) {
+                    setGangIcon(event.data.name);
+                    $("#gang").show();
+                } else
+                    $("#gang").hide();
+            }
+            setValue(event.data.key, event.data.value);
+        } else if (event.data.action == "updateStatus") {
+            updateStatus(event.data.status);
+        } else if (event.data.action == "toggle") {
+            if (event.data.show)
+                $('#ui').show();
+            else
+                $('#ui').hide();
+        } else if (event.data.action == "togglePlayerStats") {
+            if (event.data.show)
+                $('#playerStats').show();
+            else
+                $('#playerStats').hide();
+        } else if (event.data.action == "toggleMainStats") {
+            if (event.data.show)
+                $('.mainStats').show();
+            else
+                $('.mainStats').hide();
+        } else if (event.data.action == "showcarhud") {
+            if (event.data.showhud) {
+                $('.huds').fadeIn();
+                setProgressSpeed(event.data.speed, '.progress-speed');
+                setProgressFuel(event.data.fuel, '.progress-fuel');
+            } else {
+                $('.huds').fadeOut();
+            }
+        } else if (event.data.action == "setbelt") {
+            if (!event.data.isAccepted || event.data.belt) {
+                $('.belt').fadeOut();
+            } else {
+                $('.belt').fadeIn();
+            }
+        } else if (event.data.action == "copyToClipboard") {
+            let copyTextarea = document.createElement('textarea');
+            let currentSelection = document.getSelection();
 
-			copyTextarea.textContent = event.data.content;
-			document.body.appendChild(copyTextarea);
+            copyTextarea.textContent = event.data.content;
+            document.body.appendChild(copyTextarea);
 
-			currentSelection.removeAllRanges();
-			copyTextarea.select();
-			document.execCommand('copy');
+            currentSelection.removeAllRanges();
+            copyTextarea.select();
+            document.execCommand('copy');
 
-			currentSelection.removeAllRanges();
-			document.body.removeChild(copyTextarea);
-			console.log("Copy to clipboard : \n" + event.data.content);
-		}
-	});
+            currentSelection.removeAllRanges();
+            document.body.removeChild(copyTextarea);
+            console.log("Copy to clipboard : \n" + event.data.content);
+        }
+    });
 });
 
 
 
 function setValue(key, value) {
-	$('#' + key + ' span').html(value);
-	if ($('.mainStats').is(":hidden"))
-		$('.mainStats').fadeIn(500).fadeOut(2000);
+    $('#' + key + ' span').html(value);
+    if ($('.mainStats').is(":hidden"))
+        $('.mainStats').fadeIn(500).fadeOut(2000);
 
 }
 
 function setJobIcon(value) {
-	$('#job img').attr('src', 'img/jobs/' + value + '.png')
+    $('#job img').attr('src', 'img/jobs/' + value + '.png')
 }
 
 function setJob2Icon(value) {
-	$('#job2 img').attr('src', 'img/jobs/' + value + '.png')
+    $('#job2 img').attr('src', 'img/jobs/' + value + '.png')
 }
 
 function setGangIcon(value) {
-	$('#gang img').attr('src', 'img/gangs/' + value + '.png')
+    $('#gang img').attr('src', 'img/gangs/' + value + '.png')
 }
 
 function updateStatus(status) {
-	var hunger = status[0]
-	var thirst = status[1]
-	var drunk = status[2]
-	var drugged = status[3]
-	$('#hunger .bg').css('height', hunger.percent + '%')
-	$('#water .bg').css('height', thirst.percent + '%')
-	$('#drunk .bg').css('height', drunk.percent + '%');
-	$('#drugged .bg').css('height', drugged.percent + '%');
-	if (drunk.percent > 0) {
-		$('#drunk').show();
-	} else {
-		$('#drunk').hide();
-	}
-	if (drugged.percent > 0) {
-		$('#drugged').show();
-	} else {
-		$('#drugged').hide();
-	}
+    var hunger = status[0]
+    var thirst = status[1]
+    var drunk = status[2]
+    var drugged = status[3]
+    $('#hunger .bg').css('height', hunger.percent + '%')
+    $('#water .bg').css('height', thirst.percent + '%')
+    $('#drunk .bg').css('height', drunk.percent + '%');
+    $('#drugged .bg').css('height', drugged.percent + '%');
+    if (drunk.percent > 0) {
+        $('#drunk').show();
+    } else {
+        $('#drunk').hide();
+    }
+    if (drugged.percent > 0) {
+        $('#drugged').show();
+    } else {
+        $('#drugged').hide();
+    }
 
 }
 
 
 // Fuel
 function setProgressFuel(percent, element) {
-	var circle = document.querySelector(element);
-	var radius = circle.r.baseVal.value;
-	var circumference = radius * 2 * Math.PI;
-	var html = $(element).parent().parent().find('span');
+    var circle = document.querySelector(element);
+    var radius = circle.r.baseVal.value;
+    var circumference = radius * 2 * Math.PI;
+    var html = $(element).parent().parent().find('span');
 
-	circle.style.strokeDasharray = `${circumference} ${circumference}`;
-	circle.style.strokeDashoffset = `${circumference}`;
+    circle.style.strokeDasharray = `${circumference} ${circumference}`;
+    circle.style.strokeDashoffset = `${circumference}`;
 
-	const offset = circumference - ((-percent * 73) / 100) / 100 * circumference;
-	circle.style.strokeDashoffset = -offset;
+    const offset = circumference - ((-percent * 73) / 100) / 100 * circumference;
+    circle.style.strokeDashoffset = -offset;
 
-	html.text(Math.round(percent));
+    html.text(Math.round(percent));
 }
 
 // Speed
 function setProgressSpeed(value, element) {
-	var circle = document.querySelector(element);
-	var radius = circle.r.baseVal.value;
-	var circumference = radius * 2 * Math.PI;
-	var html = $(element).parent().parent().find('span');
-	var percent = value * 100 / 220;
-	if (percent > 100) percent = 100;
+    var circle = document.querySelector(element);
+    var radius = circle.r.baseVal.value;
+    var circumference = radius * 2 * Math.PI;
+    var html = $(element).parent().parent().find('span');
+    var percent = value * 100 / 220;
+    if (percent > 100) percent = 100;
 
-	circle.style.strokeDasharray = `${circumference} ${circumference}`;
-	circle.style.strokeDashoffset = `${circumference}`;
+    circle.style.strokeDasharray = `${circumference} ${circumference}`;
+    circle.style.strokeDashoffset = `${circumference}`;
 
-	const offset = circumference - ((-percent * 73) / 100) / 100 * circumference;
-	circle.style.strokeDashoffset = -offset;
+    const offset = circumference - ((-percent * 73) / 100) / 100 * circumference;
+    circle.style.strokeDashoffset = -offset;
 
-	html.text(value);
+    html.text(value);
 }
