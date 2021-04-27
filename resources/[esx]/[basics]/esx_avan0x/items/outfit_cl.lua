d0a80581b54b598cacf7ebcd5e150761caba0dcd
AvaN0x <clement.ricatte57@gmail.com>
2021-04-27 19:09:58 +0200

    edit tenueprison femme

--- resources/[esx]/[basics]/esx_avan0x/items/outfit_cl.lua
+++ resources/[esx]/[basics]/esx_avan0x/items/outfit_cl.lua
@@ -79,20 +79,21 @@ AddEventHandler('esx_avan0x:settenueprison', function()
 				}
 				TriggerEvent('skinchanger:loadClothes', skin, clothesSkin)
 			else
-				local clothesSkin = {
-					['tshirt_1']  = 14, ['tshirt_2']  = 0,
-					['torso_1']   = 73, ['torso_2']   = 0,
-					['decals_1']  = 0,  ['decals_2']  = 0,
-					['mask_1']    = 0, ['mask_2']    = 6,
-					['arms']      = 14,
-					['pants_1']   = 3, ['pants_2']   = 15,
-					['shoes_1']   = 52, ['shoes_2']   = 0,
-					['helmet_1']  = -1, ['helmet_2']  = 0,
-					['bags_1']    = 0, ['bags_2']  = 0,
-					['bproof_1']  = 0,  ['bproof_2']  = 0,
-					['chain_1']	  = 0,  ['chain_2']   = 0
-				}
-				TriggerEvent('skinchanger:loadClothes', skin, clothesSkin)
+				-- local clothesSkin = {
+				-- 	['tshirt_1']  = 14, ['tshirt_2']  = 0,
+				-- 	['torso_1']   = 73, ['torso_2']   = 0,
+				-- 	['decals_1']  = 0,  ['decals_2']  = 0,
+				-- 	['mask_1']    = 0, ['mask_2']    = 6,
+				-- 	['arms']      = 14,
+				-- 	['pants_1']   = 3, ['pants_2']   = 15,
+				-- 	['shoes_1']   = 52, ['shoes_2']   = 0,
+				-- 	['helmet_1']  = -1, ['helmet_2']  = 0,
+				-- 	['bags_1']    = 0, ['bags_2']  = 0,
+				-- 	['bproof_1']  = 0,  ['bproof_2']  = 0,
+				-- 	['chain_1']	  = 0,  ['chain_2']   = 0
+				-- }
+				-- TriggerEvent('skinchanger:loadClothes', skin, clothesSkin)
+                TriggerEvent('skinchanger:loadClothes', skin, json.decode('{"chain_2":0,"bproof_1":0,"chain_1":0,"bags_2":0,"shoes_2":12,"torso_1":247,"tshirt_1":14,"helmet_2":0,"torso_2":0,"pants_1":134,"helmet_1":-1,"arms":15,"shoes_1":72,"tshirt_2":0,"bags_1":84,"bproof_2":0,"pants_2":4}'))
 			end
 		end)
 	else
