d6d2a5fee7ccbbe5840fbd3d1531be8c29de60d4
AvaN0x <clement.ricatte57@gmail.com>
2021-05-01 16:11:41 +0200

    edit sql file

--- resources/[esx]/[jobs]/esx_ava_jobs/esx_ava_jobs.sql
+++ resources/[esx]/[jobs]/esx_ava_jobs/esx_ava_jobs.sql
@@ -1,23 +1,59 @@
-INSERT INTO `addon_inventory` (`name`, `label`, `shared`) VALUES
-	('society_vigneron','Vigneron', 1)
+INSERT INTO `addon_account` (`name`, `label`, `shared`) VALUES
+    -- vigneron
+    ('society_vigneron','Vigneron',1)
+
+    -- tailor
+    ('society_tailor','Couturier',1)
+
 ;
+
+
+INSERT INTO `inventories` (`name`, `label`, `max_weight`, `shared`) VALUES
+    -- vigneron
+    ('society_vigneron', 'Vigneron', 500000, 1),
+
+    -- tailor
+    ('society_tailor', 'Couturier', 500000, 1),
+
+;
+
+
 INSERT INTO `datastore` (`name`, `label`, `shared`) VALUES
+    -- vigneron
 	('society_vigneron', 'Vigneron', 1)
+
+    -- tailor
+	('society_tailor', 'Couturier', 1)
+
 ;
 
 INSERT INTO `jobs`(`name`, `label`, `whitelisted`) VALUES
+    -- vigneron
 	('vigneron', 'Vigneron', 1)
+
+    -- tailor
+	('tailor', 'Couturier', 1)
+
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-	('vigneron',0,'interim','Intérimaire', 100, '{"tshirt_1":59,"tshirt_2":0,"torso_1":12,"torso_2":5,"shoes_1":7,"shoes_2":2,"pants_1":9, "pants_2":7, "arms":1, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}','{"tshirt_1":0,"tshirt_2":0,"torso_1":56,"torso_2":0,"shoes_1":27,"shoes_2":0,"pants_1":36, "pants_2":0, "arms":63, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}'),
+    -- vigneron
+    ('vigneron',0,'interim','Intérimaire', 100, '{"tshirt_1":59,"tshirt_2":0,"torso_1":12,"torso_2":5,"shoes_1":7,"shoes_2":2,"pants_1":9, "pants_2":7, "arms":1, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}','{"tshirt_1":0,"tshirt_2":0,"torso_1":56,"torso_2":0,"shoes_1":27,"shoes_2":0,"pants_1":36, "pants_2":0, "arms":63, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}'),
 	('vigneron',1,'employe','Employé', 200, '{"tshirt_1":57,"tshirt_2":0,"torso_1":13,"torso_2":5,"shoes_1":7,"shoes_2":2,"pants_1":9, "pants_2":7, "arms":11, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}','{"tshirt_1":0,"tshirt_2":0,"torso_1":56,"torso_2":0,"shoes_1":27,"shoes_2":0,"pants_1":36, "pants_2":0, "arms":63, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}'),
 	('vigneron',2,'chef','Chef', 400, '{"tshirt_1":57,"tshirt_2":0,"torso_1":13,"torso_2":5,"shoes_1":7,"shoes_2":2,"pants_1":9, "pants_2":7, "arms":11, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}','{"tshirt_1":0,"tshirt_2":0,"torso_1":56,"torso_2":0,"shoes_1":27,"shoes_2":0,"pants_1":36, "pants_2":0, "arms":63, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}'),
 	('vigneron',3,'boss','Patron', 500,'{"tshirt_1":57,"tshirt_2":0,"torso_1":13,"torso_2":5,"shoes_1":7,"shoes_2":2,"pants_1":9, "pants_2":7, "arms":11, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}','{"tshirt_1":15,"tshirt_2":0,"torso_1":14,"torso_2":15,"shoes_1":12,"shoes_2":0,"pants_1":9, "pants_2":5, "arms":1, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}')
+
+    -- tailor
+	('tailor',0,'interim','Intérimaire', 50, '{"tshirt_1":59,"tshirt_2":0,"torso_1":12,"torso_2":5,"shoes_1":7,"shoes_2":2,"pants_1":9, "pants_2":7, "arms":1, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}','{"tshirt_1":0,"tshirt_2":0,"torso_1":56,"torso_2":0,"shoes_1":27,"shoes_2":0,"pants_1":36, "pants_2":0, "arms":63, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}'),
+	('tailor',1,'employe','Employé', 200, '{"tshirt_1":57,"tshirt_2":0,"torso_1":13,"torso_2":5,"shoes_1":7,"shoes_2":2,"pants_1":9, "pants_2":7, "arms":11, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}','{"tshirt_1":0,"tshirt_2":0,"torso_1":56,"torso_2":0,"shoes_1":27,"shoes_2":0,"pants_1":36, "pants_2":0, "arms":63, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}'),
+	('tailor',2,'chef','Chef', 400, '{"tshirt_1":57,"tshirt_2":0,"torso_1":13,"torso_2":5,"shoes_1":7,"shoes_2":2,"pants_1":9, "pants_2":7, "arms":11, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}','{"tshirt_1":0,"tshirt_2":0,"torso_1":56,"torso_2":0,"shoes_1":27,"shoes_2":0,"pants_1":36, "pants_2":0, "arms":63, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}'),
+	('tailor',3,'boss','Patron', 500,'{"tshirt_1":57,"tshirt_2":0,"torso_1":13,"torso_2":5,"shoes_1":7,"shoes_2":2,"pants_1":9, "pants_2":7, "arms":11, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}','{"tshirt_1":15,"tshirt_2":0,"torso_1":14,"torso_2":15,"shoes_1":12,"shoes_2":0,"pants_1":9, "pants_2":5, "arms":1, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}')
+
 ;
 
 
 INSERT INTO `items` (`name`, `label`, `limit`) VALUES
+    -- vigneron
 	('raisin', 'Raisin', 180),
 	('jus_raisin', 'Jus de raisin', 18),
 	('vine', 'Vin', 18),
@@ -27,6 +63,14 @@ INSERT INTO `items` (`name`, `label`, `limit`) VALUES
 	('vinebox', 'Caisse de vin', 20),
 	('jus_raisinbox', 'Caisse de Jus de raisin', 20),
 	('champagnebox', 'Caisse de Champagne', 20),
-	('grand_crubox', 'Caisse de Grand Cru', 20)
+	('grand_crubox', 'Caisse de Grand Cru', 20),
+
+    -- tailor
+    ('wool', 'Laine', 180),
+	('fabric', '🧵 Tissu', 72),
+	('clothe', '👔 Vêtement', 36),
+	('cardboardbox', 'Boite en carton', 20),
+	('clothebox', 'Boite de vêtements', 20)
+
 ;
 
