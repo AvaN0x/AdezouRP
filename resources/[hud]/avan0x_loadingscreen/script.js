5f70c5b827692088e95906a1c07c1026d24cef5e
AvaN0x <clement.ricatte57@gmail.com>
2021-05-04 18:05:26 +0200

    add new loading screen and randomize them

--- resources/[hud]/avan0x_loadingscreen/script.js
+++ resources/[hud]/avan0x_loadingscreen/script.js
@@ -1,4 +1,21 @@
+// https://css-tricks.com/snippets/jquery/shuffle-children/
+$.fn.shuffleChildren = function () {
+    $.each(this.get(), function (index, el) {
+        var $el = $(el);
+        var $find = $el.children();
+
+        $find.sort(function () {
+            return 0.5 - Math.random();
+        });
+
+        $el.empty();
+        $find.appendTo($el);
+    });
+};
+
+
 $(document).ready(function () {
+    $('#slideshow').shuffleChildren();
 
     var mousePos = {};
 
