2ec7352600eff187e983c156d127af87026b6e7f
AvaN0x <clement.ricatte57@gmail.com>
2021-05-05 19:40:25 +0200

    add unicorn job

--- resources/[esx]/[jobs]/esx_ava_jobs/config.lua
+++ resources/[esx]/[jobs]/esx_ava_jobs/config.lua
@@ -611,6 +611,74 @@ Config.Jobs = {
             }
         }
     },
+    unicorn = {
+        SocietyName = 'society_unicorn',
+        LabelName = 'Unicorn',
+        Blip = {
+            Sprite = 121,
+            Colour = 0
+        },
+        Zones = {
+            JobActions = {
+                Pos = vector3(132.14, -1290.15, 28.29),
+                Size  = {x = 1.5, y = 1.5, z = 1.0},
+                Color = {r = 136, g = 243, b = 216},
+                Name  = "Point d'action",
+                HelpText = _('press_to_open'),
+                Marker = 27
+            },
+            Dressing = {
+                Pos = vector3(106.71, -1299.75, 27.79),
+                Size  = {x = 1.5, y = 1.5, z = 1.0},
+                Color = {r = 136, g = 243, b = 216},
+                Name  = "Dressing",
+                HelpText = _('press_to_open'),
+                Marker = 27,
+                Blip = true
+            },
+            SocietyGarage = {
+                Name  = "Garage véhicule",
+                HelpText = _('spawn_veh'),
+                Pos = vector3(143.47, -1285.09, 29.34),
+                Size  = {x = 2.0, y = 2.0, z = 2.0},
+                Color = {r = 0, g = 255, b = 0},
+                Marker = 36,
+                Type = "car",
+                SpawnPoint = {
+                    Pos = vector3(143.47, -1285.09, 29.34),
+                    Heading = 293.0
+                },
+                Blip = true
+            },
+        },
+        BuyZones = {
+            BuyBox = {
+                Items = {
+                    {name = 'icetea', price = 15},
+                    {name = 'sprite', price = 15},
+                    {name = 'orangina', price = 15},
+                    {name = 'coffee', price = 13},
+                    {name = 'cocacola', price = 15},
+                    {name = 'ice', price = 2},
+                    {name = 'martini', price = 20},
+                    {name = 'martini2', price = 20},
+                    {name = 'rhum', price = 20},
+                    {name = 'tequila', price = 20},
+                    {name = 'vodka', price = 20},
+                    {name = 'beer', price = 20},
+                    {name = 'whisky', price = 20}
+                },
+                Pos = vector3(387.02, -343.28, 45.85),
+                Size  = {x = 1.5, y = 1.5, z = 1.5},
+                Color = {r = 136, g = 232, b = 9},
+                Name  = "Achat de boissons",
+                HelpText = _('press_buy'),
+                Marker = 27,
+                Blip = true
+            }
+        }
+    },
+
 }
 
 
