1815cd2f2fddd2ad887116598ce99bab8ce6d8eb
AvaN0x <clement.ricatte57@gmail.com>
2021-05-12 22:53:27 +0200

    few changements in dealer

--- resources/[esx]/[illegal]/avan0x_dealer/client/main.lua
+++ resources/[esx]/[illegal]/avan0x_dealer/client/main.lua
@@ -22,29 +22,38 @@ function DrawMissionText(msg, time)
 	DrawSubtitleTimed(time, 1)
 end
 
+local playerCoords = nil
+local playerPed = nil
+
+Citizen.CreateThread(function()
+	while true do
+        playerPed = PlayerPedId()
+		playerCoords = GetEntityCoords(playerPed)
+		Wait(500)
+    end
+end)
+
+
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
-		local timer = 0
-		local plyPed = PlayerPedId()
-		local coords = GetEntityCoords(plyPed)
 		if not MissionStarted then
 			local p = Config.HintLocation
-			if GetDistanceBetweenCoords(coords, p, true) < Config.DrawTextDist then
+			if #(playerCoords - p.xyz) < Config.DrawTextDist then
 				ESX.ShowHelpNotification("Appuyez sur ~INPUT_CONTEXT~ pour frapper à la porte")
+
 				if IsControlJustPressed(0, 38) then
-					timer = GetGameTimer()
-					TaskGoStraightToCoord(plyPed, p.x, p.y, p.z, 10.0, 10, p.w, 0.5)
+					TaskGoStraightToCoord(playerPed, p.x, p.y, p.z, 10.0, 10, p.w, 0.5)
 					Wait(3000)
-					ClearPedTasksImmediately(plyPed)
+					ClearPedTasksImmediately(playerPed)
 
 					while not HasAnimDictLoaded("timetable@jimmy@doorknock@") do
 						RequestAnimDict("timetable@jimmy@doorknock@")
 						Citizen.Wait(0)
 					end
-					TaskPlayAnim(plyPed, "timetable@jimmy@doorknock@", "knockdoor_idle", 8.0, 8.0, -1, 4, 0, 0, 0, 0 )
+					TaskPlayAnim(playerPed, "timetable@jimmy@doorknock@", "knockdoor_idle", 8.0, 8.0, -1, 4, 0, 0, 0, 0 )
 					Citizen.Wait(0)
-					while IsEntityPlayingAnim(plyPed, "timetable@jimmy@doorknock@", "knockdoor_idle", 3) do
+					while IsEntityPlayingAnim(playerPed, "timetable@jimmy@doorknock@", "knockdoor_idle", 3) do
 						Citizen.Wait(0)
 					end
 
@@ -52,13 +61,13 @@ Citizen.CreateThread(function()
 
 					TriggerServerEvent('3dme:shareDisplay', "* L'individu remarque un petit morceau de papier glissé sous la porte *")
 
-					ClearPedTasksImmediately(plyPed)
+					ClearPedTasksImmediately(playerPed)
 
 					local randNum = math.random(1, #Config.SalesLocations)
 					local spawnLoc = Config.SalesLocations[randNum]
 
 					MissionStarted = spawnLoc
-
+                    print(spawnLoc)
 					zoneBlip = AddBlipForRadius((spawnLoc.x + math.random(-45, 45)), (spawnLoc.y + math.random(-45, 45)), spawnLoc.z, 120.0)
 						SetBlipSprite(zoneBlip, 9)
 						SetBlipColour(zoneBlip, 1)
@@ -70,14 +79,12 @@ Citizen.CreateThread(function()
 				end
 			end
 		else
-			Citizen.Wait(500)
+			Citizen.Wait(1000)
 		end
 	end
 end)
 
 function MissionStart()
-	local plyPed = GetPlayerPed(-1)
-	local pPos = GetEntityCoords(plyPed)
 	local tPos = MissionStarted
 	local prices = {}
 	for k, v in pairs(Config.DrugItems) do
@@ -85,11 +92,9 @@ function MissionStart()
 	end
 	local startTime = GetGameTimer()
 	local timer = 600000 -- 10 minutes
-	while ((GetGameTimer() - startTime) < math.floor(timer) and not MissionCompleted) or (MissionCompleted and GetDistanceBetweenCoords(pPos, tPos, true) < 60) do
+	while ((GetGameTimer() - startTime) < math.floor(timer) and not MissionCompleted) or (MissionCompleted and #(playerCoords - tPos.xyz) < 60) do
 		Citizen.Wait(0)
-		plyPed = GetPlayerPed(-1)
-		pPos = GetEntityCoords(plyPed)
-		if GetDistanceBetweenCoords(pPos, tPos, true) < 30.0 then
+		if #(playerCoords - tPos.xyz) < 30.0 then
 			if not PedSpawned then
 				local hash = GetHashKey(Config.DealerPed)
 				while not HasModelLoaded(hash) do
@@ -104,11 +109,11 @@ function MissionStart()
 				ESX.ShowNotification("Vous êtes proche de l'acheteur")
 
 				if math.random(1, 3) == 1 then
-					TriggerServerEvent("esx_phone:sendEmergency", "police", "Une personne suspecte a été aperçue proche de cette position.", true, { ["x"] = (pPos.x + math.random(-20, 20)), ["y"] = (pPos.y + math.random(-20, 20)), ["z"] = pPos.z })
+					TriggerServerEvent("esx_phone:sendEmergency", "police", "Une personne suspecte a été aperçue proche de cette position.", true, { ["x"] = (playerCoords.x + math.random(-20, 20)), ["y"] = (playerCoords.y + math.random(-20, 20)), ["z"] = playerCoords.z })
 				end
 			end
 
-			if GetDistanceBetweenCoords(pPos, tPos, true) < Config.DrawTextDist then
+			if #(playerCoords - tPos.xyz) < Config.DrawTextDist then
 				if not MissionCompleted then
 					startTime = 0
 					MissionCompleted = true
@@ -117,44 +122,8 @@ function MissionStart()
 				ESX.ShowHelpNotification("Appuyez sur ~INPUT_CONTEXT~ pour parler au dealer")
 
 				if IsControlJustPressed(0, 38) then
-					ESX.TriggerServerCallback('avan0x_dealer:GetDrugCount', function(counts)
-						ESX.UI.Menu.CloseAll()
-						local elements = {}
-						for k, v in pairs(Config.DrugItems) do
-							drugPrice = prices[v]
-							table.insert(elements, {label = k..' : $'..drugPrice, val = v, price = drugPrice})
-						end
-						ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'Drug_Dealer', {title = "Acheteur de drogues", align = 'left', elements = elements},
-						function(data, menu)
-							local count = false
-							ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'How_Much', {title = "Combien voulez-vous vendre ? [Max : "..counts[data.current.val].."]"},
-							function(data2, menu2)
-								local quantity = tonumber(data2.value)
-
-								if quantity == nil then
-									ESX.ShowNotification("Montant non valide")
-								else
-									count = quantity
-									menu2.close()
-								end
-							end,
-							function(data2, menu2)
-								menu2.close()
-							end)
-							while not count do Citizen.Wait(0); end
-							if tonumber(count) > tonumber(counts[data.current.val]) then
-								ESX.ShowNotification("Tu n'as pas tant que ça. "..data.current.val..".")
-							else
-								TriggerServerEvent('avan0x_dealer:Sold', data.current.val, data.current.price, count)
-								menu.close()
-								Citizen.Wait(1500)
-							end
-						end,
-						function(data, menu)
-							menu.close()
-						end)
-					end)
-				end
+                    OpenDealerMenu(prices)
+                end
 			end
 		end
 	end
@@ -176,4 +145,46 @@ function MissionStart()
 		MissionStarted = false
 		PedSpawned = false
 	end
+end
+
+function OpenDealerMenu(prices)
+    ESX.TriggerServerCallback('avan0x_dealer:GetDrugCount', function(counts)
+        ESX.UI.Menu.CloseAll()
+        local elements = {}
+        for k, v in pairs(Config.DrugItems) do
+            if counts[v] > 0 or v == 'gold' or v == 'diamond' then
+                local drugPrice = prices[v]
+                table.insert(elements, {label = k..' : $'..drugPrice, val = v, price = drugPrice})
+            end
+        end
+        ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'Drug_Dealer', {title = "Dealer", align = 'left', elements = elements},
+        function(data, menu)
+            local count = false
+            ESX.UI.Menu.Open('dialog', GetCurrentResourceName(), 'How_Much', {title = "Combien voulez-vous vendre ? [Max : "..counts[data.current.val].."]"},
+            function(data2, menu2)
+                local quantity = tonumber(data2.value)
+
+                if quantity == nil then
+                    ESX.ShowNotification("Montant non valide")
+                else
+                    count = quantity
+                    menu2.close()
+                end
+            end,
+            function(data2, menu2)
+                menu2.close()
+            end)
+            while not count do Citizen.Wait(0); end
+            if tonumber(count) > tonumber(counts[data.current.val]) then
+                ESX.ShowNotification("Tu n'as pas tant que ça. "..data.current.val..".")
+            else
+                TriggerServerEvent('avan0x_dealer:Sold', data.current.val, data.current.price, count)
+                menu.close()
+                Citizen.Wait(1500)
+            end
+        end,
+        function(data, menu)
+            menu.close()
+        end)
+    end)
 end
