60fda1f84c3a8fbbcc7b5893e90ec11e7249cfbf
AvaN0x <clement.ricatte57@gmail.com>
2021-05-15 19:33:27 +0200

    useless push to test workflows

--- resources/[esx]/[jobs]/esx_ava_jobs/esx_ava_jobs.sql
+++ resources/[esx]/[jobs]/esx_ava_jobs/esx_ava_jobs.sql
@@ -18,7 +18,6 @@ INSERT INTO `addon_account` (`name`, `label`, `shared`) VALUES
 
 ;
 
-
 INSERT INTO `inventories` (`name`, `label`, `max_weight`, `shared`) VALUES
     -- vigneron
     ('society_vigneron', 'Vigneron', 500000, 1),
@@ -41,6 +40,7 @@ INSERT INTO `datastore` (`name`, `label`, `shared`) VALUES
     -- cluckin
 
 ;
+
 INSERT INTO `jobs`(`name`, `label`, `whitelisted`) VALUES
     -- vigneron
 	('vigneron', 'Vigneron', 1),
@@ -52,6 +52,7 @@ INSERT INTO `jobs`(`name`, `label`, `whitelisted`) VALUES
 	('cluckin', 'Cluckin Bell', 1),
 
 ;
+
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
     -- vigneron
     ('vigneron',0,'interim','Intérimaire', 100, '{"tshirt_1":59,"tshirt_2":0,"torso_1":12,"torso_2":5,"shoes_1":7,"shoes_2":2,"pants_1":9, "pants_2":7, "arms":1, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}','{"tshirt_1":0,"tshirt_2":0,"torso_1":56,"torso_2":0,"shoes_1":27,"shoes_2":0,"pants_1":36, "pants_2":0, "arms":63, "helmet_1":11, "helmet_2":0,"bags_1":0,"bags_2":0,"ears_1":0,"glasses_1":0,"ears_2":0,"glasses_2":0}'),
