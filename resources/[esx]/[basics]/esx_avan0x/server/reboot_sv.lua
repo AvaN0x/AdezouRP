8ae4c42e8c954520ea0dbcbc2e08527fc6290729
AvaN0x <clement.ricatte57@gmail.com>
2021-05-17 20:20:19 +0200

    edit few things about timings

--- resources/[esx]/[basics]/esx_avan0x/server/reboot_sv.lua
+++ resources/[esx]/[basics]/esx_avan0x/server/reboot_sv.lua
@@ -14,17 +14,24 @@ function checkreboot()
 	for _, hour in ipairs(rebootHours) do
 		if date_local == hour .. ':30:00' then
 			TriggerClientEvent('esx:showNotification', -1, "~r~Le serveur reboot automatiquement dans 30 minutes !")
-			ExecuteCommand('weather thunder')
+			break
+        elseif date_local == hour .. ':40:00' then
+			ExecuteCommand('weather rain')
 			ExecuteCommand('freezeweather')
 			break
 		elseif date_local == hour .. ':45:00' then
 			TriggerClientEvent('esx:showNotification', -1, "~r~Le serveur reboot automatiquement dans 15 minutes !")
-			ExecuteCommand('time 23 0')
+			break
+		elseif date_local == hour .. ':48:00' then
+			ExecuteCommand('weather thunder')
 			-- GetClockHours() is client sided
 			ExecuteCommand('freezetime')
 			break
 		elseif date_local == hour .. ':55:00' then
 			TriggerClientEvent('esx:showNotification', -1, "~r~Le serveur reboot automatiquement dans 5 minutes ! Pensez à vous déconnecter !")
+            break
+		elseif date_local == hour .. ':57:00' then
+			ExecuteCommand('time 23 0')
 			ExecuteCommand('blackout')
 			ExecuteCommand('weather halloween')
 			break
