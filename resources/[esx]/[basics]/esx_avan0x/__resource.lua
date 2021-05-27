8423ee1ef76dcb7cd3fd0d61bb6de3c46ba2e578
AvaN0x <clement.ricatte57@gmail.com>
2021-05-27 19:37:15 +0200

    new ChooseClosestPlayer

--- resources/[esx]/[basics]/esx_avan0x/__resource.lua
+++ resources/[esx]/[basics]/esx_avan0x/__resource.lua
@@ -5,12 +5,18 @@ author 'github.com/AvaN0x'
 description 'ESX AvaN0x -- random shit'
 
 client_scripts {
+    "@es_extended/locale.lua",
+    "locales/*.lua",
+    'config.lua',
     'client/main.lua',
     '**/*_cl.lua'
 }
 
 server_scripts {
     "@mysql-async/lib/MySQL.lua",
+    "@es_extended/locale.lua",
+    "locales/*.lua",
+    'config.lua',
     'server/main.lua',
     '**/*_sv.lua'
 }
@@ -19,6 +25,10 @@ data_file 'VEHICLE_METADATA_FILE' 'vehicles/*.meta'
 
 files {
 	'handling.meta'
+}
+
+export {
+    "ChooseClosestPlayer"
 }
 
 server_export {
---
+++ resources/[esx]/[basics]/esx_avan0x/locales/fr.lua
@@ -0,0 +1,4 @@
+Locales["fr"] = {
+	["select_a_player"] = "Sélectionnez une personne",
+	["no_players_near"] = "Aucun joueur proche",
+}
