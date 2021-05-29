68d5aa6df42472b9ea46d43b88c3f52c55e8c0ff
AvaN0x <clement.ricatte57@gmail.com>
2021-05-29 21:43:45 +0200

    fix locale

--- resources/[esx]/[basics]/esx_avan0x/locales/fr.lua
+++ resources/[esx]/[basics]/esx_avan0x/locales/fr.lua
@@ -1,6 +1,6 @@
 Locales["fr"] = {
 	["select_a_vehicle"] = "Sélectionnez un véhicule",
-	["no_vehicles_near"] = "Personne n'est assez proche",
+	["no_vehicles_near"] = "Aucun véhicule n'est assez proche",
 	["select_a_player"] = "Sélectionnez une personne",
 	["no_players_near"] = "Personne n'est assez proche",
 }
