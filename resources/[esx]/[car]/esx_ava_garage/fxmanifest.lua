8cb67696c9d0c900d3b1436ad9b693d22fd8e411
AvaN0x <clement.ricatte57@gmail.com>
2021-07-01 23:20:41 +0200

    fix missing dependency

--- resources/[esx]/[car]/esx_ava_garage/fxmanifest.lua
+++ resources/[esx]/[car]/esx_ava_garage/fxmanifest.lua
@@ -5,12 +5,15 @@ version '2.0.0'
 author 'github.com/AvaN0x'
 description 'Garage'
 
-server_scripts {
-    '@es_extended/locale.lua',
-	'config.lua',
-	'server.lua'
-}
+
 client_script {
 	'config.lua',
 	'client.lua'
+}
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    '@es_extended/locale.lua',
+	'config.lua',
+	'server.lua'
 }
