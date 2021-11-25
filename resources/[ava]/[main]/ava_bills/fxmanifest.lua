af1afab94a68256f54d90b02c431fa0771ad1f0d
AvaN0x <clement.ricatte57@gmail.com>
2021-11-25 14:13:43 +0100

    feat(bills): :sparkles: started working on bills, add sql file

---
+++ resources/[ava]/[main]/ava_bills/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "cerulean"
+games {"gta5"}
+
+version "1.0.0"
+author "github.com/AvaN0x"
+description "Handler for Bills"
+repository "https://github.com/AvaN0x/AdezouRP"
+
+lua54 "yes"
+
+server_only "yes"
+
+server_scripts {"server/main.lua"}
+
+dependencies {"ava_core"}
---
+++ resources/[ava]/[main]/ava_bills/server/main.lua
@@ -0,0 +1,20 @@
+-------------------------------------------
+-------- MADE BY GITHUB.COM/AVAN0X --------
+--------------- AvaN0x#6348 ---------------
+-------------------------------------------
+RegisterNetEvent("ava_bills:server:sendBillToPlayer", function(target)
+
+end)
+
+RegisterNetEvent("ava_bills:server:sendJobBillToPlayer", function(target)
+
+end)
+
+RegisterNetEvent("ava_bills:server:sendBillToJob", function(jobName)
+
+end)
+
+RegisterNetEvent("ava_bills:server:sendJobBillToJob", function(jobName)
+
+end)
+
