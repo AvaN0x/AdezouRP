e4afc17e7b3a94ccc54663801194eeca3d7dbc93
AvaN0x <clement.ricatte57@gmail.com>
2021-12-04 12:10:23 +0100

    feat: :sparkles: work with base64 images

---
+++ resources/[ava]/[main]/base64toruntime/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "cerulean"
+game "gta5"
+
+version "1.0"
+author "github.com/AvaN0x"
+description "Allows to create runtime texture from base64 encoded data"
+
+lua54 "yes"
+
+client_scripts {"client/main.lua"}
+
+ui_page "html/index.html"
+files {"html/index.html", "html/index.js"}
