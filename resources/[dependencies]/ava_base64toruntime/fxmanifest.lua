f326159c2033349837e302df9ba941031d71bd06
AvaN0x <clement.ricatte57@gmail.com>
2021-12-09 19:21:35 +0100

    move base64toruntime to dependencies

--- resources/[dependencies]/ava_base64toruntime/fxmanifest.lua
+++ resources/[dependencies]/ava_base64toruntime/fxmanifest.lua
@@ -4,6 +4,7 @@ game "gta5"
 version "1.0"
 author "github.com/AvaN0x"
 description "Allows to create runtime texture from base64 encoded data"
+repository "https://github.com/AvaN0x/AdezouRP"
 
 lua54 "yes"
 
