546c8499060a582b9dc8a5854d9e3cf1b0efd1dd
AvaN0x <clement.ricatte57@gmail.com>
2022-01-02 15:27:29 +0100

    docs(lsradio): :bulb: add comments

--- resources/[voip]/rp-radio/server.lua
+++ resources/[voip]/rp-radio/server.lua
@@ -1,5 +1,9 @@
+-------------------------------------------
+-------- MADE BY GITHUB.COM/AVAN0X --------
+--------------- AvaN0x#6348 ---------------
+-------------------------------------------
 exports.ava_core:RegisterUsableItem("radio", function(src)
     print(src, "use radio")
-    TriggerClientEvent('Radio.Set', src, true)
-	TriggerClientEvent('Radio.Toggle', src)
+    TriggerClientEvent("Radio.Set", src, true)
+    TriggerClientEvent("Radio.Toggle", src)
 end)
