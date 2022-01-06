fb9fea2c953000d5d53c1f53b0f030e2a24f3776
AvaN0x <clement.ricatte57@gmail.com>
2022-01-06 11:40:16 +0100

    refactor(mp_peds): :construction: refactor everything about how the script was thought

--- resources/[dependencies]/ava_mp_peds/client/main.lua
+++ resources/[dependencies]/ava_mp_peds/client/main.lua
@@ -6,78 +6,13 @@ local localSkin = {}
 local localPlayerSkinSave = nil
 local NumHairColors<const> = GetNumHairColors()
 
-local Categories = {
-    gender = {
-        components = {"gender"},
-        setPed = function(ped)
-            local model<const> = localSkin.gender == 0 and GetHashKey("mp_m_freemode_01") or GetHashKey("mp_f_freemode_01")
-
-            if model ~= GetEntityModel(ped) and IsModelValid(model) and IsModelInCdimage(model) then
-                local p = promise.new()
-                Citizen.CreateThread(function()
-                    RequestModel(model)
-                    while not HasModelLoaded(model) do
-                        Wait(0)
-                    end
-
-                    SetPlayerModel(PlayerId(), model)
-                    ped = PlayerPedId()
-
-                    SetModelAsNoLongerNeeded(model)
-
-                    p:resolve()
-                end)
-                Citizen.Await(p)
-            end
-            print("set default variation")
-            ClearPedDecorations(ped)
-            -- ClearPedDecorationsLeaveScars(ped)
-            SetPedDefaultComponentVariation(ped)
-            SetPedHairColor(ped, 0, 0)
-            SetPedEyeColor(ped, 0)
-            SetPedHeadBlendData(ped, 0, 0, 0, 0, 0, 0, 0.5, 0.5, 0, false);
-            ClearAllPedProps(ped, 0)
-        end,
-        -- updateMax = function()
-
-        -- end,
-    },
-    -- TODO drawable 0
-    -- TODO drawable 1
-    hair = {
-        components = {"hair", "hair_txd", "main_hair_color", "scnd_hair_color"},
-        setPed = function(ped)
-            ClearPedDecorationsLeaveScars(ped)
-            -- Hair
-            -- SetPedComponentVariation(ped, 2, localSkin.hair, localSkin.hair_txd, 2)
-            SetPedComponentVariation(ped, 2, localSkin.hair, localSkin.hair_txd, 0)
-            print(ped, 2, localSkin.hair, localSkin.hair_txd, 0)
-            -- Hair Color
-            SetPedHairColor(ped, localSkin.main_hair_color, localSkin.scnd_hair_color)
-            print(ped, localSkin.main_hair_color, localSkin.scnd_hair_color)
-
-            -- TODO Hair overlay
-        end,
-        updateMax = function(ped)
-            AVAConfig.skinComponents.hair.max = GetNumberOfPedDrawableVariations(ped, 2) - 1
-            AVAConfig.skinComponents.hair_txd.max = GetNumberOfPedTextureVariations(ped, 2, localSkin.hair) - 1
-            AVAConfig.skinComponents.main_hair_color.max = NumHairColors - 1
-            AVAConfig.skinComponents.scnd_hair_color.max = NumHairColors - 1
-        end,
-    },
-    -- gender = {
-    --     components = {"gender"},
-    --     setPed = function(ped)
-
-    --     end,
-    --     updateMax = function()
-
-    --     end,
-    -- },
-}
+-- AVAConfig.skinComponents.hair.max = GetNumberOfPedDrawableVariations(ped, 2) - 1
+-- AVAConfig.skinComponents.hair_txd.max = GetNumberOfPedTextureVariations(ped, 2, localSkin.hair) - 1
+-- AVAConfig.skinComponents.main_hair_color.max = NumHairColors - 1
+-- AVAConfig.skinComponents.scnd_hair_color.max = NumHairColors - 1
 
 -- Init local skin to default values
-for element, value in pairs(AVAConfig.skinComponents) do
+for element, value in pairs(AVAConfig.allComponents) do
     localSkin[element] = value.default or value.min
 end
 
@@ -90,7 +25,7 @@ local function saveOrRestorePlayerLocalSkin(ped)
         if localPlayerSkinSave then
             print("^1[DEBUG]^0 Restoring player skin")
             print("localPlayerSkinSave before", json.encode(localPlayerSkinSave, {indent = true}))
-            for element, value in pairs(AVAConfig.skinComponents) do
+            for element, value in pairs(AVAConfig.allComponents) do
                 localSkin[element] = localPlayerSkinSave[element] or value.default or value.min
             end
             print("localSkin after", json.encode(localSkin, {indent = true}))
@@ -103,7 +38,7 @@ local function saveOrRestorePlayerLocalSkin(ped)
             localPlayerSkinSave = {}
             print("^1[DEBUG]^0 Saving player skin")
             print("localSkin before", json.encode(localSkin, {indent = true}))
-            for element, value in pairs(AVAConfig.skinComponents) do
+            for element, value in pairs(AVAConfig.allComponents) do
                 -- Save value
                 localPlayerSkinSave[element] = localSkin[element] or value.default or value.min
                 -- Set value to default for localSkin
@@ -115,72 +50,118 @@ local function saveOrRestorePlayerLocalSkin(ped)
     end
 end
 
-local function setPedCategoryInternal(ped, category, skin)
+---Set ped components based on skin components, internal
+---@param ped entity
+---@param skin table
+local function setPedSkinInternal(ped, skin)
+    -- #region Set player model
+    local model<const> = localSkin.gender == 0 and GetHashKey("mp_m_freemode_01") or GetHashKey("mp_f_freemode_01")
+
+    if model ~= GetEntityModel(ped) and IsModelValid(model) and IsModelInCdimage(model) then
+        RequestModel(model)
+        while not HasModelLoaded(model) do
+            Wait(0)
+        end
+
+        SetPlayerModel(PlayerId(), model)
+        ped = PlayerPedId()
+
+        SetModelAsNoLongerNeeded(model)
+    end
+    -- #endregion Set player model
+
+    ClearPedDecorations(ped)
+    -- ClearPedDecorationsLeaveScars(ped)
+    SetPedDefaultComponentVariation(ped)
+    SetPedHairColor(ped, 0, 0)
+    SetPedEyeColor(ped, 0)
+    ClearAllPedProps(ped, 0)
+
+    -- #region HeadBlend
+    SetPedHeadBlendData(ped, 0, 0, 0, 0, 0, 0, 0.5, 0.5, 0, false)
+    while not HasPedHeadBlendFinished(ped) do
+        Wait(0)
+    end
+    -- #endregion HeadBlend
+    -- TODO drawable 0
+    -- TODO drawable 1
+
+    -- Hair
+    SetPedComponentVariation(ped, 2, localSkin.hair, localSkin.hair_txd, 0)
+    -- Hair Color
+    SetPedHairColor(ped, localSkin.main_hair_color, localSkin.scnd_hair_color)
+
+    -- Hair overlays and tattoos
+    reloadPedOverlays(ped)
+end
+
+---Set ped components based on clothes components, internal
+---@param ped entity
+---@param skin table
+local function setPedClothesInternal(ped, skin)
+
+end
+
+-------------
+-- EXPORTS --
+-------------
+
+---Set ped components based on all components
+---@param ped entity
+---@param skin table
+function setPedAllComponents(ped, skin)
+    saveOrRestorePlayerLocalSkin(ped)
+
     if skin then
-        for i = 0, #category.components do
-            local component<const> = category.components[i]
+        for component, _ in pairs(AVAConfig.allComponents) do
             if skin[component] then
                 localSkin[component] = skin[component]
             end
         end
     end
 
-    print("Applying skin category with " .. json.encode(category.components) .. " to " .. ped)
-    category.setPed(ped)
-    if category.updateMax then
-        category.updateMax(ped)
-    end
+    setPedSkinInternal(ped, skin)
+    setPedClothesInternal(ped, skin)
 end
+exports("setPedAllComponents", setPedAllComponents)
 
----Set a ped component from a category
----@param ped entity
----@param category string
----@param skin table
-function setPedSkinWithCategory(ped, category, skin)
-    saveOrRestorePlayerLocalSkin(ped)
-
-    if Categories[category] then
-        if category == "gender" and ped ~= PlayerPedId() then
-            print("^1[ERROR] An error occured, you cannot change the gender of a non player ped.^0")
-            return
-        end
-        setPedCategoryInternal(ped, Categories[category], skin)
-    end
-end
-exports("setPedSkinWithCategory", setPedSkinWithCategory)
-
----Set ped components based on all categories
+---Set ped components based on skin components
 ---@param ped entity
 ---@param skin table
 function setPedSkin(ped, skin)
     saveOrRestorePlayerLocalSkin(ped)
 
-    -- TODO start with gender
-    print("Setting skin to " .. ped)
-    if ped == PlayerPedId() then
-        setPedCategoryInternal(ped, Categories["gender"], skin)
-        ped = PlayerPedId()
-    end
-    for categoryName, category in pairs(Categories) do
-        if categoryName ~= "gender" then
-            setPedCategoryInternal(ped, category, skin)
+    if skin then
+        for i = 0, #AVAConfig.skinComponents do
+            local component<const> = AVAConfig.skinComponents[i]
+            if skin[component] then
+                localSkin[component] = skin[component]
+            end
         end
     end
+
+    setPedSkinInternal(ped, skin)
 end
 exports("setPedSkin", setPedSkin)
 
----Apply ped components from the skin array
+---Set ped components based on clothes components
 ---@param ped entity
 ---@param skin table
-function applyPedSkin(ped, skin)
+function setPedClothes(ped, skin)
     saveOrRestorePlayerLocalSkin(ped)
-    local categories = {}
 
-    -- go on every element from skin, add the category to categories and set it
-    -- TODO start with gender
+    if skin then
+        for i = 0, #AVAConfig.clothesComponents do
+            local component<const> = AVAConfig.clothesComponents[i]
+            if skin[component] then
+                localSkin[component] = skin[component]
+            end
+        end
+    end
 
+    setPedClothesInternal(ped, skin)
 end
-exports("applyPedSkin", applyPedSkin)
+exports("setPedClothes", setPedClothes)
 
 ---Returns player skin
 ---@return table
@@ -188,3 +169,21 @@ function getPlayerSkin()
     return localPlayerSkinSave or localSkin
 end
 exports("getPlayerSkin", getPlayerSkin)
+
+---Reload ped overlays, hairs and tattoos
+---@param ped entity
+function reloadPedOverlays(ped)
+    ClearPedDecorations(ped)
+
+    if AVAConfig.HairOverlays[localSkin.gender] then
+        local overlay<const> = AVAConfig.HairOverlays[localSkin.gender][localSkin.hair]
+        if overlay and overlay.collection and overlay.overlay then
+            print("^3[DEBUG]^0 Set hair overlays", overlay.collection, overlay.overlay)
+            AddPedDecorationFromHashes(ped, overlay.collection, overlay.overlay)
+        end
+    end
+
+    -- TODO tattoos
+end
+exports("reloadPlayerOverlays", reloadPlayerOverlays)
+
