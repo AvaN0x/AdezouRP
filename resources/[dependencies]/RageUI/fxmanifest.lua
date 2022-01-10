0dcc4eb54b9af70b8d88fa191455221ea4ee0f8d
AvaN0x <clement.ricatte57@gmail.com>
2022-01-10 17:20:24 +0100

    fix(rageui): :bug: fix fxmanifest

--- resources/[dependencies]/RageUI/fxmanifest.lua
+++ resources/[dependencies]/RageUI/fxmanifest.lua
@@ -3,25 +3,20 @@
 --- @version 1.0.0
 --- created at [24/05/2021 10:02]
 ---
-
 -- edited by AvaN0x
 -- BIG THANKS to https://github.com/iTexZoz for the work done on this
-fx_version 'cerulean'
+fx_version "cerulean"
 
-game 'gta5';
+game "gta5"
 
 client_scripts {
-	"src/RageUI.lua",
-	"src/Menu.lua",
-	"src/MenuController.lua",
-	"src/components/*.lua",
-	"src/elements/*.lua",
-	"src/items/*.lua",
-	"src/panels/*.lua",
-	"src/windows/*.lua",
-	"example.lua"
+    "src/RageUI.lua",
+    "src/Menu.lua",
+    "src/MenuController.lua",
+    "src/components/*.lua",
+    "src/elements/*.lua",
+    "src/items/*.lua",
+    "src/windows/*.lua",
+    "example.lua",
 }
-
-
-
 
