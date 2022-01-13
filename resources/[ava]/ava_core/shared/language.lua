95fcdd6cc6c1dc52db58eb8bb118124910db543e
AvaN0x <clement.ricatte57@gmail.com>
2022-01-13 22:13:11 +0100

    docs(core): :memo: add documentation for GetString

--- resources/[ava]/ava_core/shared/language.lua
+++ resources/[ava]/ava_core/shared/language.lua
@@ -6,14 +6,18 @@ local language = GetConvar("ava_core_language", "en")
 local file = LoadResourceFile(GetCurrentResourceName(), ("languages/%s.json"):format(language))
 local strings = file and json.decode(file) or {}
 
-function GetString(string, ...)
+---Get string from language files
+---@param str string "string name"
+---@param ... any "format arguments"
+---@return string
+function GetString(str, ...)
     if not strings then
         print("^1[strings missing]^0 strings have not already loaded.")
-        return string
-    elseif strings[string] then
-        return string.format(strings[string], ...)
+        return str
+    elseif strings[str] then
+        return string.format(strings[str], ...)
     else
-        print("^1[String missing] ^0strings[\"^6" .. string .. "^0\"] does not exist in ^6" .. language .. "^0.")
-        return string
+        print("^1[String missing] ^0strings[\"^6" .. str .. "^0\"] does not exist in ^6" .. language .. "^0.")
+        return str
     end
 end
