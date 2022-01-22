825f06b9ff73f9e0d0d77dfeddedf50df512d73b
AvaN0x <clement.ricatte57@gmail.com>
2022-01-22 18:44:49 +0100

    feat(items): :sparkles: introduce ava_items, introduce vehicle lockpicking

---
+++ resources/[ava]/[main]/ava_items/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "cerulean"
+game "gta5"
+
+version "1.0"
+author "github.com/AvaN0x"
+description "Items"
+repository "https://github.com/AvaN0x/AdezouRP"
+
+lua54 "yes"
+
+files {"languages/**.json"}
+
+client_scripts {"config.lua", "client/main.lua", "client/**/*_cl.lua"}
+
+server_scripts {"config.lua", "server/main.lua", "server/**/*_sv.lua"}
+
+shared_scripts {"@ava_core/shared/language.lua"}
+
+dependencies {"ava_core"}
+
