f4afce05f8e4050488a80cd940d72c6c641fbf42
AvaN0x <clement.ricatte57@gmail.com>
2022-01-27 11:18:19 +0100

    refactor(core): :recycle: now using AddTextComponentString for drawtext3d

--- resources/[ava]/ava_core/client/utils.lua
+++ resources/[ava]/ava_core/client/utils.lua
@@ -520,13 +520,12 @@ AVA.Utils.DrawText3D = function(x, y, z, text, size, r, g, b)
     local onScreen, _x, _y = World3dToScreen2d(x, y, z)
 
     if onScreen then
-        AddTextEntry("AVA_DRW_3DTXT", text)
-
         SetTextScale(0.35, size or 0.35)
         SetTextFont(0)
         SetTextProportional(1)
         SetTextColour(r or 255, g or 255, b or 255, 215)
-        SetTextEntry("AVA_DRW_3DTXT")
+        SetTextEntry("STRING")
+        AddTextComponentString(text)
         SetTextCentre(1)
         SetTextOutline()
 
