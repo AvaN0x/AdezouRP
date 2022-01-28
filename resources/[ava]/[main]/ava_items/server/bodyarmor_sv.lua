30bcd83fc0c969074bb1075d3eb6ddeaf6fb502c
AvaN0x <clement.ricatte57@gmail.com>
2022-01-28 14:06:34 +0100

    feat(items): :sparkles: add bodyarmor item

---
+++ resources/[ava]/[main]/ava_items/server/bodyarmor_sv.lua
@@ -0,0 +1,15 @@
+-------------------------------------------
+-------- MADE BY GITHUB.COM/AVAN0X --------
+--------------- AvaN0x#6348 ---------------
+-------------------------------------------
+exports.ava_core:RegisterUsableItem("bodyarmor", function(source)
+    TriggerClientEvent("ava_items:client:useBodyarmor", source)
+end)
+
+RegisterNetEvent("ava_items:server:removeBodyArmor", function()
+    local aPlayer = exports.ava_core:GetPlayer(source)
+    if aPlayer then
+        local inventory = aPlayer.getInventory()
+        inventory.removeItem("bodyarmor", 1)
+    end
+end)
