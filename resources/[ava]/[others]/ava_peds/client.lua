2624c75dad299e1dd5e8e0884cfe28a9c2a40b28
AvaN0x <clement.ricatte57@gmail.com>
2022-04-09 22:53:38 +0200

    fix(mp_peds): :bug: fix DrawText3D

--- resources/[ava]/[others]/ava_peds/client.lua
+++ resources/[ava]/[others]/ava_peds/client.lua
@@ -110,13 +110,12 @@ function DrawText3D(x, y, z, text, size, r, g, b)
     local onScreen, _x, _y = World3dToScreen2d(x, y, z)
 
     if onScreen then
-        AddTextEntry("AVA_DRW_3DTXT", text)
-
         SetTextScale(0.35, size or 0.35)
         SetTextFont(0)
         SetTextProportional(1)
         SetTextColour(r or 255, g or 255, b or 255, 215)
-        SetTextEntry("AVA_DRW_3DTXT")
+        SetTextEntry("STRING")
+        AddTextComponentSubstringPlayerName(text)
         SetTextCentre(1)
         SetTextOutline()
 
