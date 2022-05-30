eb853fa9058b6294a161612f3e7af6017c0acd9d
AvaN0x <clement.ricatte57@gmail.com>
2022-05-30 19:06:20 +0200

    feat(chairs): :wrench: add new chair

--- resources/[ava]/[others]/ava_chairs/config.lua
+++ resources/[ava]/[others]/ava_chairs/config.lua
@@ -7,15 +7,16 @@ Config = {}
 Config.Key = 23 -- F (38 is E)
 
 Config.Props = {
-    {hash = -1531508740, offX = 0.0, offY = 0.0, offZ = -1.0, offHeading = 180.0, type = "Chair"},
-    {hash = GetHashKey("v_med_bed1"), offX = 0.0, offY = 0.0, offZ = -0.8, offHeading = 180.0, type = "Bed"},
-    {hash = GetHashKey("v_med_bed2"), offX = 0.0, offY = 0.0, offZ = -0.8, offHeading = 0.0, type = "Bed"},
-    {hash = GetHashKey("prop_bench_01a"), offX = 0.0, offY = 0.0, offZ = -0.4, offHeading = 180.0, type = "Chair"},
-    {hash = GetHashKey("prop_busstop_02"), offX = 0.0, offY = 0.5, offZ = -0.4, offHeading = 180.0, type = "Chair"},
-    {hash = GetHashKey("v_corp_offchair"), offX = 0.0, offY = 0.05, offZ = -0.4, offHeading = 180.0, type = "Chair"},
-    {hash = -853526657, offX = 0.0, offY = 0.05, offZ = -1.0, offHeading = 180.0, type = "Chair"}, -- ls custom office
-    {hash = 558578166, offX = 0.0, offY = 0.05, offZ = -0.9, offHeading = 180.0, type = "Chair"},
-    {hash = 1840174940, offX = 0.0, offY = 0.05, offZ = -0.1, offHeading = 180.0, type = "Chair"}, -- gabz vu bar
+    { hash = -1531508740, offX = 0.0, offY = 0.0, offZ = -1.0, offHeading = 180.0, type = "Chair" },
+    { hash = GetHashKey("v_med_bed1"), offX = 0.0, offY = 0.0, offZ = -0.8, offHeading = 180.0, type = "Bed" },
+    { hash = GetHashKey("v_med_bed2"), offX = 0.0, offY = 0.0, offZ = -0.8, offHeading = 0.0, type = "Bed" },
+    { hash = GetHashKey("prop_bench_01a"), offX = 0.0, offY = 0.0, offZ = -0.4, offHeading = 180.0, type = "Chair" },
+    { hash = GetHashKey("prop_busstop_02"), offX = 0.0, offY = 0.5, offZ = -0.4, offHeading = 180.0, type = "Chair" },
+    { hash = GetHashKey("v_corp_offchair"), offX = 0.0, offY = 0.05, offZ = -0.4, offHeading = 180.0, type = "Chair" },
+    { hash = -853526657, offX = 0.0, offY = 0.05, offZ = -1.0, offHeading = 180.0, type = "Chair" }, -- ls custom office
+    { hash = 558578166, offX = 0.0, offY = 0.05, offZ = -0.9, offHeading = 180.0, type = "Chair" },
+    { hash = 1840174940, offX = 0.0, offY = 0.05, offZ = -0.1, offHeading = 180.0, type = "Chair" }, -- gabz vu bar
+    { hash = -1633198649, offX = 0.0, offY = 0.05, offZ = -0.35, offHeading = 180.0, type = "Chair" }, -- freedmanh city hall, gov chair
 }
 
 Config.Anims = {
@@ -32,8 +33,14 @@ Config.Anims = {
         },
     },
     Bed = {
-        Male = {dict = "switch@trevor@annoys_sunbathers", anim = "trev_annoys_sunbathers_loop_guy"},
-        Female = {dict = "switch@trevor@annoys_sunbathers", anim = "trev_annoys_sunbathers_loop_guy"},
+        Male = {
+            dict = "switch@trevor@annoys_sunbathers",
+            anim = "trev_annoys_sunbathers_loop_guy"
+        },
+        Female = {
+            dict = "switch@trevor@annoys_sunbathers",
+            anim = "trev_annoys_sunbathers_loop_guy"
+        },
     },
 }
 
