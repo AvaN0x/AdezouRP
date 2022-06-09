34a91554f1bbfaaf99888ea4c22d1d935ff5a16d
AvaN0x <clement.ricatte57@gmail.com>
2022-06-09 22:00:26 +0200

    fix(jobs): :bug: few fixes for lumberjack job

--- resources/[ava]/[main]/ava_jobs/config/job/lumberjack.lua
+++ resources/[ava]/[main]/ava_jobs/config/job/lumberjack.lua
@@ -48,33 +48,33 @@ Config.Jobs.lumberjack = {
     FieldZones = {
         WoodField = {
             Items = { { name = "woodpile", quantity = 1 } },
-            PropHash = GetHashKey("prop_logpile_03"),
+            -- PropHash = GetHashKey("prop_logpile_03"), -- TODO: TOO BIG
+            PropHash = GetHashKey("prop_mk_race_chevron_02"),
             Coord = vector3(-513.54, 5242.46, 79.32),
             MinGroundHeight = 78,
             MaxGroundHeight = 80,
             Name = "1. Récolte",
-            Radius = 2 ,
             Blip = true,
         },
     },
     ProcessZones = {
         WoodProcess = {
             ItemsGive = { { name = "woodpile", quantity = 1 } },
-            ItemsGet = { { name = "woodchipsbag", quantity = 50 } },
+            ItemsGet = { { name = "woodchipsbag", quantity = 10 } },
             Delay = 6000,
             Scenario = "WORLD_HUMAN_CLIPBOARD", -- https://pastebin.com/6mrYTdQv
             Coord = vector3(-580.32, 5277.04, 69.28),
             Size = { x = 2.5, y = 2.5, z = 1.5 },
             Color = { r = 252, g = 186, b = 3 },
-            Name = "2. Brouillage",
+            Name = "2. Broyage",
             HelpText = GetString("press_traitement"),
             Marker = 27,
             Blip = true,
         },
     },
     SellZones = {
-        woodSell = {
-            Items = { { name = "woodchipsbag", price = 1400 } },
+        WoodSell = {
+            Items = { { name = "woodchipsbag", price = 140 } },
             Coord = vector3(618.96, 2785.04, 42.50),
             Size = { x = 1.5, y = 1.5, z = 1.5 },
             Color = { r = 252, g = 186, b = 3 },
@@ -88,7 +88,7 @@ Config.Jobs.lumberjack = {
         -- Job garages
         {
             Name = "jobgarage_lumberjack",
-            Coord = vector3(-509.77, 5266.37, 79.63),
+            Coord = vector3(-509.77, 5266.37, 80.65),
             Size = { x = 2.0, y = 2.0, z = 2.0 },
             Color = { r = 252, g = 186, b = 3 },
             Marker = 36,
