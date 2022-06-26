dfcabe4e66ac292d039ef8a03904da48df45d0a9
AvaN0x <clement.ricatte57@gmail.com>
2022-06-26 15:23:05 +0200

    fix(status): :bug: fix fast freeze of ped while fading out

--- resources/[ava]/[main]/ava_status/client/animations.lua
+++ resources/[ava]/[main]/ava_status/client/animations.lua
@@ -8,7 +8,8 @@ IsLongAnimated = false
 function SetLongAnimated(prop)
     Citizen.CreateThread(function()
         IsLongAnimated = true
-        local instructionalButtons = exports.ava_core:GetScaleformInstructionalButtons({{control = "~INPUT_VEH_DUCK~", label = GetString("cancel_animation")}})
+        local instructionalButtons = exports.ava_core:GetScaleformInstructionalButtons({ { control = "~INPUT_VEH_DUCK~",
+            label = GetString("cancel_animation") } })
 
         while IsLongAnimated do
             Wait(0)
@@ -47,7 +48,8 @@ RegisterNetEvent("ava_status:client:eat", function(prop_name)
             local x, y, z = table.unpack(GetEntityCoords(playerPed))
             local prop = CreateObject(GetHashKey(prop_name), x, y, z + 0.2, true, true, true)
             local boneIndex = GetPedBoneIndex(playerPed, 18905)
-            AttachEntityToEntity(prop, playerPed, boneIndex, 0.13, 0.052, 0.031, -70.0, 175.0, 90.0, true, true, false, true, 1, true)
+            AttachEntityToEntity(prop, playerPed, boneIndex, 0.13, 0.052, 0.031, -70.0, 175.0, 90.0, true, true, false,
+                true, 1, true)
 
             exports.ava_core:RequestAnimDict("mp_player_inteat@burger")
             TaskPlayAnim(playerPed, "mp_player_inteat@burger", "mp_player_int_eat_burger_fp", 8.0, -8, -1, 49, 0, 0, 0, 0)
@@ -72,7 +74,8 @@ RegisterNetEvent("ava_status:client:drink", function(prop_name)
             local x, y, z = table.unpack(GetEntityCoords(playerPed))
             local prop = CreateObject(GetHashKey(prop_name), x, y, z + 0.2, true, true, true)
             local boneIndex = GetPedBoneIndex(playerPed, 18905)
-            AttachEntityToEntity(prop, playerPed, boneIndex, 0.15, 0.018, 0.031, -100.0, 55.0, 350.0, true, true, false, true, 1, true)
+            AttachEntityToEntity(prop, playerPed, boneIndex, 0.15, 0.018, 0.031, -100.0, 55.0, 350.0, true, true, false,
+                true, 1, true)
 
             exports.ava_core:RequestAnimDict("mp_player_intdrink")
             TaskPlayAnim(playerPed, "mp_player_intdrink", "loop_bottle", 8.0, -8, -1, 49, 0, 0, 0, 0)
@@ -97,7 +100,8 @@ RegisterNetEvent("ava_status:client:smoke", function()
             local x, y, z = table.unpack(GetEntityCoords(playerPed))
             local prop = CreateObject(GetHashKey("p_amb_joint_01"), x, y, z + 0.2, true, true, true)
             local boneIndex = GetPedBoneIndex(playerPed, 18905)
-            AttachEntityToEntity(prop, playerPed, boneIndex, 0.145, 0.038, 0.045, 0.0, 0.0, 80.0, true, true, false, true, 1, true)
+            AttachEntityToEntity(prop, playerPed, boneIndex, 0.145, 0.038, 0.045, 0.0, 0.0, 80.0, true, true, false, true
+                , 1, true)
 
             exports.ava_core:RequestAnimDict("amb@world_human_smoking_pot@male@base")
             TaskPlayAnim(playerPed, "amb@world_human_smoking_pot@male@base", "base", 8.0, -8, -1, 49, 0, 0, 0, 0)
@@ -138,7 +142,7 @@ function ResetPlayerStatusEffects()
     Citizen.CreateThread(function()
         local playerPed = PlayerPedId()
 
-        DoScreenFadeOut(800)
+        DoScreenFadeOut(1200)
         Wait(1000)
 
         ClearTimecycleModifier()
